e358d395d38041062e36fea4326702c384ae6a03
Ioannis Tsakpinis <iotsakp@gmail.com>
2013-12-01 20:01:19 +0200

    New generated class: org.lwjgl.opencl.Info. Provides convenient info queries for OpenCL objects. Should be used with a static import.

---
+++ src/core/org/lwjgl/opencl/InfoQueryObject.java
@@ -0,0 +1,205 @@
+/* 
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opencl;
+
+import org.lwjgl.LWJGLUtil;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.APIBuffer;
+
+import static org.lwjgl.Pointer.*;
+import static org.lwjgl.opencl.CLUtil.*;
+import static org.lwjgl.system.APIUtil.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * Base class for OpenCL object information queries.
+ * <p/>
+ * All methods require the object being queried (a pointer value), a second object argument (another pointer value) and the integer parameter name.
+ *
+ * @see org.lwjgl.opencl.Info
+ */
+abstract class InfoQueryObject {
+
+	protected abstract int get(long pointer, long arg, int param_name, long param_value_size, long param_value, long param_value_size_ret);
+
+	InfoQueryObject() {
+	}
+
+	/**
+	 * Returns the integer value for the given {@code param_name}, converted to a boolean.
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 *
+	 * @return the parameter's boolean value
+	 */
+	boolean getBoolean(long object, long arg, int param_name) {
+		return getInt(object, arg, param_name) != 0;
+	}
+
+	/**
+	 * Returns the integer value for the given {@code param_name}.
+	 * <p/>
+	 * For integer parameters that may be 32 or 64 bits (e.g. {@code size_t}), {@link #getPointer} should be used instead.
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 *
+	 * @return the parameter's int value
+	 */
+	int getInt(long object, long arg, int param_name) {
+		APIBuffer __buffer = apiBuffer();
+		int errcode = get(object, arg, param_name, 4L, __buffer.address(), NULL);
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+		return __buffer.intValue(0);
+	}
+
+	/**
+	 * Returns the long value for the given {@code param_name}.
+	 * <p/>
+	 * For integer parameters that may be 32 or 64 bits (e.g. {@code size_t}), {@link #getPointer} should be used instead.
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 *
+	 * @return the parameter's long value
+	 */
+	long getLong(long object, long arg, int param_name) {
+		APIBuffer __buffer = apiBuffer();
+		int errcode = get(object, arg, param_name, 8L, __buffer.address(), NULL);
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+		return __buffer.longValue(0);
+	}
+
+	/**
+	 * Returns the pointer value for the given {@code param_name}.
+	 * <p/>
+	 * This method should also be used for integer parameters that may be 32 or 64 bits (e.g. {@code size_t}).
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 *
+	 * @return the parameter's pointer value
+	 */
+	long getPointer(long object, long arg, int param_name) {
+		APIBuffer __buffer = apiBuffer();
+		int errcode = get(object, arg, param_name, POINTER_SIZE, __buffer.address(), NULL);
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+		return __buffer.pointerValue(0);
+	}
+
+	/**
+	 * Writes the pointer list for the given {@code param_name} into {@code target}.
+	 * <p/>
+	 * This method should also be used for integer parameters that may be 32 or 64 bits (e.g. {@code size_t}).
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 * @param target     the buffer in which to put the returned pointer list
+	 *
+	 * @return how many pointers were actually returned
+	 */
+	int getPointers(long object, long arg, int param_name, PointerBuffer target) {
+		APIBuffer __buffer = apiBuffer();
+		int errcode = get(object, arg, param_name, target.remaining() * POINTER_SIZE, memAddress(target), __buffer.address());
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+		return (int)(__buffer.pointerValue(0) >> POINTER_SHIFT);
+	}
+
+	/**
+	 * Returns the string value for the given {@code param_name}. The raw bytes returned are assumed to be ASCII encoded.
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 *
+	 * @return the parameter's string value
+	 */
+	String getStringASCII(long object, long arg, int param_name) {
+		APIBuffer __buffer = apiBuffer();
+		int bytes = getString(object, arg, param_name, __buffer);
+		return __buffer.stringValueASCII(0, bytes);
+	}
+
+	/**
+	 * Returns the string value for the given {@code param_name}. The raw bytes returned are assumed to be ASCII encoded and have length equal to {@code
+	 * param_value_size}.
+	 *
+	 * @param object           the object to query
+	 * @param arg              an object argument
+	 * @param param_name       the parameter to query
+	 * @param param_value_size the explicit string length
+	 *
+	 * @return the parameter's string value
+	 */
+	String getStringASCII(long object, long arg, int param_name, int param_value_size) {
+		APIBuffer __buffer = apiBuffer();
+		int errcode = get(object, arg, param_name, param_value_size, __buffer.address(), NULL);
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+		return __buffer.stringValueASCII(0, param_value_size);
+	}
+
+	/**
+	 * Returns the string value for the given {@code param_name}. The raw bytes returned are assumed to be UTF-8 encoded.
+	 *
+	 * @param object     the object to query
+	 * @param arg        an object argument
+	 * @param param_name the parameter to query
+	 *
+	 * @return the parameter's string value
+	 */
+	String getStringUTF8(long object, long arg, int param_name) {
+		APIBuffer __buffer = apiBuffer();
+		int bytes = getString(object, arg, param_name, __buffer);
+		return __buffer.stringValueUTF8(0, bytes);
+	}
+
+	/**
+	 * Returns the string value for the given {@code param_name}. The raw bytes returned are assumed to be UTF-8 encoded and have length equal to {@code
+	 * param_value_size}.
+	 *
+	 * @param object           the object to query
+	 * @param arg              an object argument
+	 * @param param_name       the parameter to query
+	 * @param param_value_size the explicit string length
+	 *
+	 * @return the parameter's string value
+	 */
+	String getStringUTF8(long object, long arg, int param_name, int param_value_size) {
+		APIBuffer __buffer = apiBuffer();
+		int errcode = get(object, arg, param_name, param_value_size, __buffer.address(), NULL);
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+		return __buffer.stringValueUTF8(0, param_value_size);
+	}
+
+	private int getString(long object, long arg, int param_name, APIBuffer __buffer) {
+		// Get string length
+		int errcode = get(object, arg, param_name, 0, NULL, __buffer.address());
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+
+		int bytes = (int)__buffer.pointerValue(0);
+		__buffer.bufferParam(bytes + POINTER_SIZE);
+
+		// Get string
+		errcode = get(object, arg, param_name, bytes, __buffer.address(), NULL);
+		if ( LWJGLUtil.DEBUG )
+			checkCLError(errcode);
+
+		return bytes - 1; // all OpenCL char[] parameters are null-terminated
+	}
+
+}
---
+++ src/templates/org/lwjgl/opencl/Info.kt
@@ -0,0 +1,197 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opencl
+
+import org.lwjgl.generator.*
+import org.lwjgl.opencl.InfoQueryType.*
+import java.util.EnumSet
+
+enum class InfoQueryType {
+	BOOLEAN
+	INT
+	LONG
+	POINTER
+	STRING_ASCII
+	STRING_UTF8
+}
+
+fun info() {
+	Generator.register(
+		CustomClass(OPENCL_PACKAGE, "Info", {
+			javaImport(
+				"org.lwjgl.PointerBuffer",
+				"static org.lwjgl.opencl.CL10.*",
+				"static org.lwjgl.opencl.CL10GL.*",
+				"static org.lwjgl.opencl.CL12.*",
+				"static org.lwjgl.opencl.CL20.*"
+			)
+
+			documentation =
+			"""
+			This class provides methods that can be used to query information about OpenCL objects. These methods are similar to the corresponding
+			{@code clGet&lt;Type&gt;Info} function for each object type, except that only a single value is returned. Which one of these methods should be used
+			depends on the type of the information being queried.
+			"""
+		}) {
+			print(
+				"""public final class Info {
+
+	private Info() {
+	}
+"""
+			)
+
+			open [data] class ObjectType(
+				val source: String,
+				val name: String,
+				val types: EnumSet<InfoQueryType>,
+				val info: String = name,
+				val param: String = name.toLowerCase()
+			) {
+				open fun getQueryImpl(field: String, info: String): String =
+					"""private static final InfoQuery $field = new InfoQuery() {
+		@Override
+		protected int get(long pointer, int param_name, long param_value_size, long param_value, long param_value_size_ret) {
+			return nclGet${info}Info(pointer, param_name, param_value_size, param_value, param_value_size_ret);
+		}
+	};"""
+				open val args: String get() = "long $param, int param_name"
+				open val argsPass: String get() = "$param, param_name"
+			}
+
+			class ObjectTypeCustom(
+				source: String,
+				name: String,
+				types: EnumSet<InfoQueryType>,
+				info: String = name,
+				param: String = name.toLowerCase(),
+			    val argName: String,
+			    val argType: String,
+			    val queryType: String
+			): ObjectType(source, name, types, info, param) {
+				override fun getQueryImpl(field: String, info: String): String =
+					"""private static final $queryType $field = new $queryType() {
+		@Override
+		protected int get(long pointer, $argType arg, int param_name, long param_value_size, long param_value, long param_value_size_ret) {
+			return nclGet${info}Info(pointer, arg, param_name, param_value_size, param_value, param_value_size_ret);
+		}
+	};"""
+				override val args: String get() = "long $param, $argType $argName, int param_name"
+				override val argsPass: String get() = "$param, $argName, param_name"
+			}
+
+			// Defines the info queries we'll generate and which types will be supported for each one.
+			// This list should be updated whenever a new OpenCL version is released.
+			val objects = array(
+				ObjectType("CL10", "Platform", EnumSet.of(STRING_ASCII, STRING_UTF8)),
+				ObjectType("CL10", "Device", EnumSet.allOf(javaClass<InfoQueryType>())),
+				ObjectType("CL10", "Context", EnumSet.of(INT, POINTER)),
+				ObjectType("CL10", "Command Queue", EnumSet.of(INT, POINTER), "CommandQueue", "command_queue"),
+				ObjectType("CL10", "Mem Object", EnumSet.of(BOOLEAN, INT, LONG, POINTER), "MemObject", "memobj"),
+				ObjectType("CL10", "Image", EnumSet.of(INT, POINTER)),
+				ObjectType("CL20", "Pipe", EnumSet.of(INT)),
+				ObjectType("CL10", "Program", EnumSet.of(INT, POINTER, STRING_ASCII, STRING_UTF8)),
+				ObjectTypeCustom(
+					"CL10", "Program Build", EnumSet.of(INT, POINTER, STRING_ASCII, STRING_UTF8), "ProgramBuild", "program",
+					argName = "device", argType = "long", queryType = "InfoQueryObject"
+				),
+				ObjectType("CL10", "Kernel", EnumSet.of(INT, POINTER, STRING_ASCII, STRING_UTF8)),
+				ObjectTypeCustom(
+					"CL10", "Kernel WorkGroup", EnumSet.of(LONG, POINTER), "KernelWorkGroup", "kernel",
+				    argName = "device", argType = "long", queryType = "InfoQueryObject"
+				),
+				ObjectTypeCustom(
+					"CL12", "Kernel Arg", EnumSet.of(INT, LONG, STRING_ASCII, STRING_UTF8), "KernelArg", "kernel",
+					argName = "arg_indx", argType = "int", queryType = "InfoQueryInt"
+				),
+				ObjectType("CL10", "Sampler", EnumSet.of(BOOLEAN, INT, POINTER)),
+				ObjectType("CL10", "Event", EnumSet.of(INT, POINTER)),
+				ObjectType("CL10", "Event Profiling", EnumSet.of(LONG), "EventProfiling", "event"),
+				ObjectType("CL10GL", "GL Texture", EnumSet.of(INT), "GLTexture", "memobj")
+			)
+
+			for ( obj in objects ) {
+				val (source, name, types, info) = obj
+				val QUERY = name.replace(' ', '_').toUpperCase()
+				val link = "{@link $source#clGet${info}Info Get${info}Info}"
+
+				print(
+					"""
+	// ------------------------------------
+	// $name ($source.clGet${info}Info)
+	// ------------------------------------
+
+	${obj.getQueryImpl(QUERY, info)}
+"""
+				)
+
+				if ( types.contains(BOOLEAN) ) {
+					print(
+						"""
+	/** Single boolean value version of: $link */
+	public static boolean clGet${info}InfoBoolean(${obj.args}) { return $QUERY.getBoolean(${obj.argsPass}); }
+"""
+					)
+				}
+
+				if ( types.contains(INT) ) {
+					print(
+						"""
+	/** Single int value version of: $link */
+	public static int clGet${info}InfoInt(${obj.args}) { return $QUERY.getInt(${obj.argsPass}); }
+"""
+					)
+				}
+
+				if ( types.contains(LONG) ) {
+					print(
+						"""
+	/** Single long value version of: $link */
+	public static long clGet${info}InfoLong(${obj.args}) { return $QUERY.getLong(${obj.argsPass}); }
+"""
+					)
+				}
+
+				if ( types.contains(POINTER) ) {
+					print(
+						"""
+	/** Single pointer value version of: $link */
+	public static long clGet${info}InfoPointer(${obj.args}) { return $QUERY.getPointer(${obj.argsPass}); }
+
+	/** PointBuffer version of: $link */
+	public static int clGet${info}InfoPointers(${obj.args}, PointerBuffer target) { return $QUERY.getPointers(${obj.argsPass}, target); }
+"""
+					)
+				}
+
+				if ( types.contains(STRING_ASCII) ) {
+					print(
+						"""
+	/** String version of: $link */
+	public static String clGet${info}InfoStringASCII(${obj.args}) { return $QUERY.getStringASCII(${obj.argsPass}); }
+
+	/** String with explicit length version of: $link*/
+	public static String clGet${info}InfoStringASCII(${obj.args}, int param_value_size) { return $QUERY.getStringASCII(${obj.argsPass}, param_value_size); }
+"""
+					)
+				}
+
+				if ( types.contains(STRING_UTF8) ) {
+					print(
+						"""
+	/** UTF-8 string version of: $link */
+	public static String clGet${info}InfoStringUTF8(${obj.args}) { return $QUERY.getStringUTF8(${obj.argsPass}); }
+
+	/** UTF-8 string with explicit length version of: $link */
+	public static String clGet${info}InfoStringUTF8(${obj.args}, int param_value_size) { return $QUERY.getStringUTF8(${obj.argsPass}, param_value_size); }
+"""
+					)
+				}
+			}
+
+			print("\n}")
+		}
+	)
+}
