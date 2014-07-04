92d1d6b82f9f3678a3ef0c5fee72e2b75c25d749
Ioannis Tsakpinis <iotsakp@gmail.com>
2014-07-04 21:40:52 +0300

    Fixed OpenGL reference page URLs; they have been converted to HTML5.

--- src/templates/org/lwjgl/generator/opengl/JavaDoc.kt
+++ src/templates/org/lwjgl/generator/opengl/JavaDoc.kt
@@ -21,7 +21,7 @@ class ReferenceGL(val function: String): FunctionModifier() {
 }
 
 fun PrintWriter.printOpenGLJavaDoc(documentation: String, function: String, deprecated: Boolean) {
-	val link = url("http://www.opengl.org/sdk/docs/man${if ( deprecated ) "2" else ""}/xhtml/$function.xml", "OpenGL SDK Reference")
+	val link = url("http://www.opengl.org/sdk/docs/man${if ( deprecated ) "2" else ""}/html/$function.xhtml", "OpenGL SDK Reference")
 	val injectedJavaDoc =
 		if ( deprecated )
 			"$link - <em>This function is deprecated and unavailable in the Core profile</em>"
