f5b8e0471dbfcbf96d5d2b2fd7a9a02fffdbb94e
Ioannis Tsakpinis <iotsakp@gmail.com>
2014-08-19 22:07:47 +0300

    Moved GLX_ARB_create_context_profile to its own template.

---
+++ src/templates/org/lwjgl/opengl/templates/GLX_ARB_create_context_profile.kt
@@ -0,0 +1,44 @@
+/* 
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengl.*
+import org.lwjgl.system.linux.*
+
+val GLX_ARB_create_context_profile = "GLXARBCreateContextProfile".nativeClassGLX("GLX_ARB_create_context_profile", ARB) {
+	javaImport(
+		"org.lwjgl.system.linux.*"
+	)
+
+	documentation =
+		"""
+		Native bindings to the ${registryLink("ARB", "glx_create_context")} extension.
+
+		Adds an attribute to ##GLXARBCreateContext, specifying the GL profile requested for a context of OpenGL 3.2 or later.
+
+		Requires ${GLX14.glx} and ${GL32.core}.
+		"""
+
+	IntConstant.block(
+		"Accepted as an attribute name in {@code attrib_list}.",
+
+		"CONTEXT_PROFILE_MASK_ARB" _ 0x9126
+	)
+
+	IntConstant.block(
+		"Accepted as bits in the attribute value for #CONTEXT_PROFILE_MASK_ARB in {@code attrib_list}.",
+
+		"CONTEXT_CORE_PROFILE_BIT_ARB" _ 0x00000001,
+		"CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB" _ 0x00000002
+	)
+
+	IntConstant.block(
+		"GLX error.",
+
+		"GLXBadProfileARB" _ 13
+	).noPrefix()
+
+}
