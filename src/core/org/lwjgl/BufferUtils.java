af494c25a3c020b976ba30eca60444cdf628920c
Ioannis Tsakpinis <iotsakp@gmail.com>
2014-11-25 01:37:52 +0200

    Better BufferUtils documentation

--- src/core/org/lwjgl/BufferUtils.java
+++ src/core/org/lwjgl/BufferUtils.java
@@ -9,7 +9,35 @@ import java.nio.*;
 import static org.lwjgl.system.MathUtil.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
-/** Some often-used Buffer code for creating native buffers of the appropriate size. */
+/**
+ * <p>This class makes it easy and safe to work with direct buffers. It is the recommended way to allocate memory to use with LWJGL.</p>
+ * <h3>Direct buffers</h3>
+ * <p>LWJGL requires that all NIO buffers passed to it are direct buffers. Direct buffers essentially wrap an address that points to off-heap memory, i.e. a
+ * native pointer. This is the only way LWJGL can safely pass data from Java code to native code, and vice-versa, without a performance penalty. It does not
+ * support on-heap Java arrays (or plain NIO buffers, which wrap them) because arrays may be moved around in memory by the JVM's garbage collector while native
+ * code is accessing them. In addition, Java arrays have an unspecified layout, i.e. they are not necessarily contiguous in memory.</p>
+ * <h3>Usage</h3>
+ * <p>When a direct buffer is passed as an argument to an LWJGL method, no data is copied. Instead, the current buffer position is added to the buffer's memory
+ * address and the resulting value is passed to native code. The native code interprets that value as a pointer and reads or copies from it as necessary. LWJGL
+ * will often also use the current buffer limit (via {@link Buffer#remaining()}) to automatically pass length/maxlength arguments. This means that, just like
+ * other APIs that use NIO buffers, the current {@link Buffer#position()} and {@link Buffer#limit()} at the time of the call is very important. Contrary to
+ * other APIs, LWJGL never modifies the current position, it will be the same value before and after the call.</p>
+ * <h3>Arrays of pointers</h3>
+ * <p>In addition to the standard NIO buffer classes, LWJGL provides a {@link PointerBuffer} class for storing pointer data in an architecture independent way.
+ * It is used in bindings for pointer-to-pointers arguments, usually to provide arrays of data (input parameter) or to store returned pointer values (output
+ * parameter).</p>
+ * <h3>Memory alignment</h3>
+ * <p>This class also supports automatically aligned buffer allocations. The {@code org.lwjgl.util.BufferAlign} system property can be used to control the
+ * alignment size. Note that the JVM guarantees a minimum alignment size of 8 bytes, which is the default. Supported values:
+ * <ul>
+ * <li><b>page</b> &ndash; OS page alignment (usually 4kb)</li>
+ * <li><b>cache-line</b> &ndash; CPU cache-line alignment (usually 64 bytes)</li>
+ * <li><b>default</b> &ndash; the default alignment (8 bytes)</li>
+ * <li><em>&lt;custom&gt;</em> &ndash; user-specified alignment, must be a power-of-two integer greater than 8</li>
+ * </ul>
+ * The system property will be read only once, when this class is loaded. Hence, the automatic alignment size cannot change at runtime, but there are additional
+ * methods that can be used when special alignment is required.</p>
+ */
 public final class BufferUtils {
 
 	private interface BufferAllocator {
@@ -68,7 +96,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-ordered bytebuffer with the specified capacity.
+	 * Allocates a direct native-ordered bytebuffer with the specified capacity.
 	 *
 	 * @param capacity The capacity, in bytes
 	 *
@@ -79,7 +107,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-order shortbuffer with the specified number
+	 * Allocates a direct native-order shortbuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in shorts
@@ -91,7 +119,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-order charbuffer with the specified number
+	 * Allocates a direct native-order charbuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in chars
@@ -103,7 +131,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-order intbuffer with the specified number
+	 * Allocates a direct native-order intbuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in ints
@@ -115,7 +143,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-order longbuffer with the specified number
+	 * Allocates a direct native-order longbuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in longs
@@ -127,7 +155,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-order floatbuffer with the specified number
+	 * Allocates a direct native-order floatbuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in floats
@@ -139,7 +167,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a direct native-order doublebuffer with the specified number
+	 * Allocates a direct native-order doublebuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in floats
@@ -151,7 +179,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Construct a PointerBuffer with the specified number
+	 * Allocates a PointerBuffer with the specified number
 	 * of elements.
 	 *
 	 * @param capacity The capacity, in memory addresses
@@ -165,7 +193,7 @@ public final class BufferUtils {
 	// Unaligned mallocs
 
 	/**
-	 * Allocates a direct ByteBuffer in native order with the given capacity.
+	 * Allocates a direct ByteBuffer in native order with the specified capacity.
 	 *
 	 * @param capacity the ByteBuffer capacity
 	 *
@@ -178,7 +206,7 @@ public final class BufferUtils {
 	// Aligned mallocs
 
 	/**
-	 * Allocates a direct ByteBuffer in native order with the given capacity and memory alignment.
+	 * Allocates a direct ByteBuffer in native order with the specified capacity and memory alignment.
 	 *
 	 * @param capacity  the ByteBuffer capacity
 	 * @param alignment the desired memory alignment, in bytes. Must be a power-of-two.
@@ -200,7 +228,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Allocates a direct ByteBuffer in native order with the given capacity. The returned ByteBuffer
+	 * Allocates a direct ByteBuffer in native order with the specified capacity. The returned ByteBuffer
 	 * will be page-aligned.
 	 *
 	 * @param capacity the ByteBuffer capacity
@@ -212,7 +240,7 @@ public final class BufferUtils {
 	}
 
 	/**
-	 * Allocates a direct ByteBuffer in native order with the given capacity. The returned ByteBuffer
+	 * Allocates a direct ByteBuffer in native order with the specified capacity. The returned ByteBuffer
 	 * will be cache-line-aligned.
 	 *
 	 * @param capacity the ByteBuffer capacity
@@ -225,39 +253,67 @@ public final class BufferUtils {
 
 	// memsets
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(ByteBuffer b) {
-		memSet(memAddress(b), 0, b.remaining());
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(ByteBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining());
 	}
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(ShortBuffer b) {
-		memSet(memAddress(b), 0, b.remaining() << 1);
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(ShortBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining() << 1);
 	}
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(CharBuffer b) {
-		memSet(memAddress(b), 0, b.remaining() << 1);
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(CharBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining() << 1);
 	}
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(IntBuffer b) {
-		memSet(memAddress(b), 0, b.remaining() << 2);
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(IntBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining() << 2);
 	}
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(FloatBuffer b) {
-		memSet(memAddress(b), 0, b.remaining() << 2);
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(FloatBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining() << 2);
 	}
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(LongBuffer b) {
-		memSet(memAddress(b), 0, b.remaining() << 3);
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(LongBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining() << 3);
 	}
 
-	/** Fill buffer with zeros from position to remaining */
-	public static void zeroBuffer(DoubleBuffer b) {
-		memSet(memAddress(b), 0, b.remaining() << 3);
+	/**
+	 * Fills the specified buffer with zeros from the current position to the current limit.
+	 *
+	 * @param buffer the buffer to fill with zeros
+	 */
+	public static void zeroBuffer(DoubleBuffer buffer) {
+		memSet(memAddress(buffer), 0, buffer.remaining() << 3);
 	}
 
 }
