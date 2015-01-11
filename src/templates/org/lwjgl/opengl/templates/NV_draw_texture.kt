a62028ee84151380d693b04b460bf99777a66060
Kai Burjack <kburjack@googlemail.com>
2015-01-11 15:14:56 +0100

    Added extensions: NV_draw_texture and NV_shader_thread_group

---
+++ src/templates/org/lwjgl/opengl/templates/NV_draw_texture.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengl.*
+
+val NV_draw_texture = "NVDrawTexture".nativeClassGL("NV_draw_texture", postfix = NV) {
+	nativeImport (
+		"OpenGL.h"
+	)
+
+	documentation =
+		"""
+		Native bindings to the $registryLink extension.
+
+		This extension provides a new function, DrawTextureNV(), allowing
+    applications to draw an screen-aligned rectangle displaying some or all of
+    the contents of a two-dimensional or rectangle texture.  Callers specify a
+    texture object, an optional sampler object, window coordinates of the
+    rectangle to draw, and texture coordinates corresponding to the corners of
+    the rectangle.  For each fragment produced by the rectangle, DrawTextureNV
+    interpolates the texture coordinates, performs a texture lookup, and uses
+    the texture result as the fragment color.
+		"""
+
+	GLvoid.func(
+		"DrawTextureNV",
+		"",
+
+		GLuint.IN("texture", ""),
+		GLuint.IN("sampler", ""),
+		GLfloat.IN("x0", ""),
+		GLfloat.IN("y0", ""),
+		GLfloat.IN("x1", ""),
+		GLfloat.IN("y1", ""),
+		GLfloat.IN("z", ""),
+		GLfloat.IN("s0", ""),
+		GLfloat.IN("t0", ""),
+		GLfloat.IN("s1", ""),
+		GLfloat.IN("t1", "")
+	)
+}
---
+++ src/templates/org/lwjgl/opengl/templates/NV_shader_thread_group.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengl.*
+
+val NV_shader_thread_group = "NVShaderThreadGroup".nativeClassGL("NV_shader_thread_group", postfix = NV) {
+	nativeImport (
+		"OpenGL.h"
+	)
+
+	documentation =
+		"""
+		Native bindings to the $registryLink extension.
+
+		Implementations of the OpenGL Shading Language may, but are not required 
+    to, run multiple shader threads for a single stage as a SIMD thread group, 
+    where individual execution threads are assigned to thread groups in an 
+    undefined, implementation-dependent order.  This extension provides a set 
+    of new features to the OpenGL Shading Language to query thread states and 
+    to share data between fragments within a 2x2 pixel quad.
+    
+    Requires ${GL43.core}.
+		"""
+
+	IntConstant.block(
+		"Accepted by the {@code pname} parameter of GetBooleanv, GetIntegerv, GetFloatv, and GetDoublev.",
+
+		"WARP_SIZE_NV" _ 0x9339,
+		"WARPS_PER_SM_NV" _ 0x933A,
+		"SM_COUNT_NV" _ 0x933B
+	)
+}
