a4a6e42284523686a379c4af0d25c94407bf427e
httpdigest <kburjack@googlemail.com>
2015-04-08 21:21:22 +0200

    Added demo to showcase glUniform3 with uniform vec3 array

---
+++ res/demo/uniformarray-fs.glsl
@@ -0,0 +1,10 @@
+#version 130
+
+uniform vec3 cols[4];
+uniform int chosen;
+
+out vec4 color;
+
+void main(void) {
+	color = vec4(cols[chosen], 1.0);
+}
---
+++ res/demo/uniformarray-vs.glsl
@@ -0,0 +1,7 @@
+#version 130
+
+in vec2 position;
+
+void main(void) {
+	gl_Position = vec4(position, 0.0, 1.0);
+}
