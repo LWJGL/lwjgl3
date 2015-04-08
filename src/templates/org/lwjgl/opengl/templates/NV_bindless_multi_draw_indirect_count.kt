70cdb83aa1d514b2604ef8e7582feb6b35176e9c
httpdigest <kburjack@googlemail.com>
2015-04-08 21:16:50 +0200

    Added demo to showcase glUniform3 with uniform vec3 array

--- src/templates/org/lwjgl/opengl/templates/NV_bindless_multi_draw_indirect_count.kt
+++ src/templates/org/lwjgl/opengl/templates/NV_bindless_multi_draw_indirect_count.kt
@@ -31,7 +31,7 @@ val NV_bindless_multi_draw_indirect_count = "NVBindlessMultiDrawIndirectCount".n
 
 		GLenum.IN("mode", "the primitive mode", PRIMITIVE_TYPES),
 		const _ GLvoid_p.IN("indirect", "an array of DrawArraysIndirectBindlessCommandNV structures (see the extension spec for more information)"),
-		GLintptr.IN("drawCount", "the draw count, which must be a multiple of four"),
+		GLintptr.IN("drawCount", "the byte offset into the buffer object containing the draw count. This offset must be a multiple of 4"),
 		GLsizei.IN("maxDrawCount", "the maximum number of draws that are expected to be stored in the buffer"),
 		GLsizei.IN("stride", "the size of one DrawArraysIndirectBindlessCommandNV structure"),
 		GLint.IN("vertexBufferCount", "the number of vertex buffers in the DrawArraysIndirectBindlessCommandNV structure")
@@ -49,7 +49,7 @@ val NV_bindless_multi_draw_indirect_count = "NVBindlessMultiDrawIndirectCount".n
 		GLenum.IN("mode", "the primitive mode", PRIMITIVE_TYPES),
 		GLenum.IN("type", "the data type of the element indices", "GL11#UNSIGNED_BYTE GL11#UNSIGNED_SHORT GL11#UNSIGNED_INT"),
 		const _ GLvoid_p.IN("indirect", "an array of DrawElementsIndirectBindlessCommandNV structures (see the extension spec for more information)"),
-		GLintptr.IN("drawCount", "the draw count, which must be a multiple of four"),
+		GLintptr.IN("drawCount", "the byte offset into the buffer object containing the draw count. This offset must be a multiple of 4"),
 		GLsizei.IN("maxDrawCount", "the maximum number of draws that are expected to be stored in the buffer"),
 		GLsizei.IN("stride", "the size of one DrawElementsIndirectBindlessCommandNV structure"),
 		GLint.IN("vertexBufferCount", "the number of vertex buffers in the DrawElementsIndirectBindlessCommandNV structure")
---
+++ src/tests/org/lwjgl/demo/opengl/UniformArrayDemo.java
@@ -0,0 +1,272 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.demo.opengl;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.GLContext;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.demo.util.IOUtil.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class UniformArrayDemo {
+
+	private long window;
+	private int width = 1024;
+	private int height = 768;
+
+	private int vao;
+	private int program;
+	private int vec3ArrayUniform;
+	private int chosenUniform;
+	private int chosen = 0;
+	private FloatBuffer colors = BufferUtils.createFloatBuffer(3 * 4);
+	{
+		colors.put(new float[] { 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0 });
+		colors.flip();
+	}
+
+	GLContext ctx;
+	GLFWErrorCallback errCallback;
+	GLFWKeyCallback keyCallback;
+	GLFWFramebufferSizeCallback fbCallback;
+
+	private void init() throws IOException {
+		glfwSetErrorCallback(errCallback = new GLFWErrorCallback() {
+			private GLFWErrorCallback delegate = Callbacks
+					.errorCallbackPrint(System.err);
+
+			@Override
+			public void invoke(int error, long description) {
+				if (error == GLFW_VERSION_UNAVAILABLE)
+					System.err
+							.println("This demo requires OpenGL 3.0 or higher.");
+				delegate.invoke(error, description);
+			}
+
+			@Override
+			public void release() {
+				delegate.release();
+				super.release();
+			}
+		});
+
+		if (glfwInit() != GL_TRUE)
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+		window = glfwCreateWindow(width, height, "Uniform array test", NULL,
+				NULL);
+		if (window == NULL) {
+			throw new AssertionError("Failed to create the GLFW window");
+		}
+		System.out
+				.println("Press 'up' or 'down' to cycle through some colors.");
+
+		glfwSetFramebufferSizeCallback(window,
+				fbCallback = new GLFWFramebufferSizeCallback() {
+					@Override
+					public void invoke(long window, int width, int height) {
+						if (width > 0
+								&& height > 0
+								&& (UniformArrayDemo.this.width != width || UniformArrayDemo.this.height != height)) {
+							UniformArrayDemo.this.width = width;
+							UniformArrayDemo.this.height = height;
+						}
+					}
+				});
+
+		glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+			@Override
+			public void invoke(long window, int key, int scancode, int action,
+					int mods) {
+				if (action != GLFW_RELEASE)
+					return;
+
+				if (key == GLFW_KEY_ESCAPE) {
+					glfwSetWindowShouldClose(window, GL_TRUE);
+				} else if (key == GLFW_KEY_UP) {
+					chosen = (chosen + 1) % 4;
+				} else if (key == GLFW_KEY_DOWN) {
+					chosen = (chosen + 1) % 4;
+				}
+			}
+		});
+
+		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - width) / 2,
+				(GLFWvidmode.height(vidmode) - height) / 2);
+		glfwMakeContextCurrent(window);
+		glfwSwapInterval(0);
+		glfwShowWindow(window);
+		ctx = GLContext.createFromCurrent();
+
+		/* Create all needed GL resources */
+		createVao();
+		createRasterProgram();
+		initProgram();
+	}
+
+	/**
+	 * Simple fullscreen quad.
+	 */
+	private void createVao() {
+		this.vao = glGenVertexArrays();
+		int vbo = glGenBuffers();
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		ByteBuffer bb = BufferUtils.createByteBuffer(4 * 2 * 6);
+		FloatBuffer fv = bb.asFloatBuffer();
+		fv.put(-1.0f).put(-1.0f);
+		fv.put(1.0f).put(-1.0f);
+		fv.put(1.0f).put(1.0f);
+		fv.put(1.0f).put(1.0f);
+		fv.put(-1.0f).put(1.0f);
+		fv.put(-1.0f).put(-1.0f);
+		glBufferData(GL_ARRAY_BUFFER, bb, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0L);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+
+	/**
+	 * Create a shader object from the given classpath resource.
+	 *
+	 * @param resource
+	 *            the class path
+	 * @param type
+	 *            the shader type
+	 *
+	 * @return the shader object id
+	 *
+	 * @throws IOException
+	 */
+	private static int createShader(String resource, int type)
+			throws IOException {
+		int shader = glCreateShader(type);
+
+		ByteBuffer source = ioResourceToByteBuffer(resource, 8192);
+
+		PointerBuffer strings = BufferUtils.createPointerBuffer(1);
+		IntBuffer lengths = BufferUtils.createIntBuffer(1);
+
+		strings.put(0, source);
+		lengths.put(0, source.remaining());
+
+		glShaderSource(shader, strings, lengths);
+		glCompileShader(shader);
+		int compiled = glGetShaderi(shader, GL_COMPILE_STATUS);
+		String shaderLog = glGetShaderInfoLog(shader);
+		if (!shaderLog.trim().isEmpty()) {
+			System.err.println(shaderLog);
+		}
+		if (compiled == 0) {
+			throw new AssertionError("Could not compile shader");
+		}
+		return shader;
+	}
+
+	/**
+	 * Create the raster shader.
+	 *
+	 * @throws IOException
+	 */
+	private void createRasterProgram() throws IOException {
+		int program = glCreateProgram();
+		int vshader = createShader("demo/uniformarray-vs.glsl",
+				GL_VERTEX_SHADER);
+		int fshader = createShader("demo/uniformarray-fs.glsl",
+				GL_FRAGMENT_SHADER);
+		glAttachShader(program, vshader);
+		glAttachShader(program, fshader);
+		glBindAttribLocation(program, 0, "position");
+		glBindFragDataLocation(program, 0, "color");
+		glLinkProgram(program);
+		int linked = glGetProgrami(program, GL_LINK_STATUS);
+		String programLog = glGetProgramInfoLog(program);
+		if (!programLog.trim().isEmpty()) {
+			System.err.println(programLog);
+		}
+		if (linked == 0) {
+			throw new AssertionError("Could not link program");
+		}
+		this.program = program;
+	}
+
+	/**
+	 * Initialize the shader program.
+	 */
+	private void initProgram() {
+		glUseProgram(this.program);
+		vec3ArrayUniform = glGetUniformLocation(this.program, "cols");
+		chosenUniform = glGetUniformLocation(this.program, "chosen");
+		glUseProgram(0);
+	}
+
+	private void render() {
+		glUseProgram(this.program);
+
+		/* Set uniform array. */
+		glUniform3fv(vec3ArrayUniform, colors);
+		/* Set chosen color (index into array) */
+		glUniform1i(chosenUniform, chosen);
+
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+		glUseProgram(0);
+	}
+
+	private void loop() {
+		while (glfwWindowShouldClose(window) == GL_FALSE) {
+			glfwPollEvents();
+			glViewport(0, 0, width, height);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			render();
+
+			glfwSwapBuffers(window);
+		}
+	}
+
+	private void run() {
+		try {
+			init();
+			loop();
+
+			errCallback.release();
+			keyCallback.release();
+			fbCallback.release();
+			glfwDestroyWindow(window);
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			glfwTerminate();
+		}
+	}
+
+	public static void main(String[] args) {
+		new UniformArrayDemo().run();
+	}
+
+}
