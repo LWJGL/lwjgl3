e8ff52845361932d2cbb4afe72b40c720140684e
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2015-04-28 19:30:57 +0200

    Added comments to OpenAl, OpenCL and OpenGL exceptions

--- src/core/org/lwjgl/opencl/OpenCLException.java
+++ src/core/org/lwjgl/opencl/OpenCLException.java
@@ -5,27 +5,54 @@
 package org.lwjgl.opencl;
 
 /**
- * A runtime exception thrown by LWJGL when it encounters an OpenCL error. The checks that
- * trigger this exception are only enabled when {@link org.lwjgl.LWJGLUtil#DEBUG} is true.
+ * A runtime exception thrown by LWJGL when it encounters an OpenCL error. The
+ * checks that trigger this exception are only enabled when
+ * {@link org.lwjgl.LWJGLUtil#DEBUG} is true.
  */
 public class OpenCLException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+        /**
+         * The serialVersionUID is a universal version identifier for a Serializable
+         * class.
+         */
+        private static final long serialVersionUID = 1L;
 
-	public OpenCLException() {
-		super();
-	}
+        /**
+         * Default constructor for <code>OpenCLException</code>.
+         */
+        public OpenCLException() {
+            super();
+        }
 
-	public OpenCLException(String message) {
-		super(message);
-	}
+        /**
+         * Constructor for OpenCLException. Constructs an instance of
+         * <code>OpenCLException</code> with the specified detail message.
+         *
+         * @param message the detail message.
+         */
+        public OpenCLException(String message) {
+            super(message);
+        }
 
-	public OpenCLException(String message, Throwable cause) {
-		super(message, cause);
-	}
+        /**
+         * Constructor for OpenCLException. Constructs an instance of
+         * <code>OpenCLException</code> with the specified detail message and cause.
+         *
+         * @param message String identifier for exception
+         * @param cause the cause of the exception
+         */
+        public OpenCLException(String message, Throwable cause) {
+            super(message, cause);
+        }
 
-	public OpenCLException(Throwable cause) {
-		super(cause);
-	}
-
+        /**
+         * Constructor for OpenCLException. Constructs an instance of
+         * <code>OpenCLException</code> with the specified cause.
+         *
+         * @param cause the cause of the exception
+         */
+        public OpenCLException(Throwable cause) {
+            super(cause);
+        }
 }
+
