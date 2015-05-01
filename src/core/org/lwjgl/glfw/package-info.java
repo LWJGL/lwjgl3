0618c10e165d745a5e1a0ebb6a79d34113f9c84d
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2015-05-01 10:50:28 +0200

    Added some package-info documentation

---
+++ src/core/org/lwjgl/glfw/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the classes and interfaces necessary for GLFW.
+ *
+ * <p>
+ * OpenGL FrameWork (GLFW) is an Open Source, lightweight utility library use
+ * with OpenGL.</p>
+ *
+ * @see <a href="http://www.glfw.org/">glfw.org</a>
+ */
+package core.org.lwjgl.glfw;
---
+++ src/core/org/lwjgl/opengl/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides the classes and interfaces necessary for OpenGL.
+ *
+ * <p>
+ * OpenGL (Open Graphics Library) is a cross-language, cross-platform API
+ * designed for rendering 2D and 3D vector graphics.</p>
+ *
+ * @see <a href="https://www.opengl.org/">opengl.org</a>
+ */
+package core.org.lwjgl.opengl;
