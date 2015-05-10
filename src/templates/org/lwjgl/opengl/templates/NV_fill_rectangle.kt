84f22ab72f990ea16997898672064e3abce731a0
httpdigest <kburjack@googlemail.com>
2015-05-10 15:33:33 +0200

    Add NV_fill_rectangle extension

---
+++ src/templates/org/lwjgl/opengl/templates/NV_fill_rectangle.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengl.*
+
+val NV_fill_rectangle = "NVFillRectangle".nativeClassGL("NV_fill_rectangle", postfix = NV) {
+	nativeImport (
+		"OpenGL.h"
+	)
+
+	documentation =
+		"""
+		Native bindings to the $registryLink extension.
+
+		This extension adds a new PolygonMode setting where a triangle is
+    rasterized by computing and filling its axis-aligned screen-space bounding
+    box, disregarding the actual triangle edges. This can be useful for
+    drawing a rectangle without being split into two triangles with an
+    internal edge.  It is also useful to minimize the number of primitives
+    that need to be drawn, particularly for a user-interface.
+		"""
+
+	IntConstant.block(
+		"Accepted by the {@code mode} parameter of PolygonMode.",
+
+		"FILL_RECTANGLE_NV" _ 0x933C
+	)
+}
