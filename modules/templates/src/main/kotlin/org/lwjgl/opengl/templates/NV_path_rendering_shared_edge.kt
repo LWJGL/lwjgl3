335eb550a859d700fab88ac370fd47fed96ca935
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-07-17 20:33:23 +0300

    Fix NV_path_rendering_shared_edge template

--- modules/templates/src/main/kotlin/org/lwjgl/opengl/templates/NV_path_rendering_shared_edge.kt
+++ modules/templates/src/main/kotlin/org/lwjgl/opengl/templates/NV_path_rendering_shared_edge.kt
@@ -8,61 +8,23 @@ import org.lwjgl.generator.*
 import org.lwjgl.opengl.*
 
 val NV_path_rendering_shared_edge = "NVPathRenderingSharedEdge".nativeClassGL("NV_path_rendering_shared_edge", postfix = NV) {
-	nativeImport (
-		"OpenGL.h"
-	)
-
 	documentation =
 		"""
 		Native bindings to the $registryLink extension.
 
-		This extension introduces a new path command modifier to the
-    NV_path_rendering extension to indicate that a path command represents an
-    edge (either straight or curved) that is shared with another path.
-    
-        Requires ${registryLinkTo("NV", "path_rendering")}
+		This extension introduces a new path command modifier to the NV_path_rendering extension to indicate that a path command represents an edge (either
+		straight or curved) that is shared with another path.
+
+		When used in conjunction with ${NV_framebuffer_mixed_samples.link}, a shared edge (or a whole path including shared edges) will use modified
+		rasterization rules in order to ensure that groups of raster samples associated with a given coverage sample will all produce consistent coverage
+		results, in order to avoid artifacts described further in the issues section at the end of this document.
+
+        Requires ${registryLinkTo("NV", "path_rendering")}.
 		"""
 
 	IntConstant(
-		"""
-		Accepted by the {@code cap} parameter of Enable, Disable, and IsEnabled, by the {@code pname} parameter of GetBooleanv, GetIntegerv, GetFloatv, and
-		GetDoublev, and by the {@code target} parameter of TexEnvi, TexEnviv, TexEnvf, TexEnvfv, GetTexEnviv, and GetTexEnvfv.
-		""",
+		"Allowed to be added to command tokens in elements of the {@code commands} array parameter of PathCommandsNV and PathSubCommandsNV.",
 
-		"POINT_SPRITE_NV" _ 0x8861
-	)
-
-	IntConstant(
-		"""
-		When the {@code target} parameter of TexEnvf, TexEnvfv, TexEnvi, TexEnviv, GetTexEnvfv, or GetTexEnviv is POINT_SPRITE_NV, then the value of
-		{@code pname} may be.
-		""",
-
-		"COORD_REPLACE_NV" _ 0x8862
-	)
-
-	IntConstant(
-		"""
-		Accepted by the {@code pname} parameter of PointParameteriNV, PointParameterfEXT, PointParameterivNV, PointParameterfvEXT, GetBooleanv, GetIntegerv,
-		GetFloatv, and GetDoublev.
-		""",
-
-		"POINT_SPRITE_R_MODE_NV" _ 0x8863
-	)
-
-	GLvoid(
-		"PointParameteriNV",
-		"",
-
-		GLenum.IN("pname", ""),
-		GLint.IN("param", "")
-	)
-
-	GLvoid(
-		"PointParameterivNV",
-		"",
-
-		GLenum.IN("pname", ""),
-		const _ Check(1) _ GLint_p.IN("params", "")
+		"SHARED_EDGE_NV" _ 0xC0
 	)
 }
