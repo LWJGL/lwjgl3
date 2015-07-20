9927f09f716ec8434d03b7c7d0a313731bc85c0f
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-07-21 01:54:30 +0300

    Add GLX extensions

---
+++ modules/templates/src/main/kotlin/org/lwjgl/opengl/templates/GLX_NV_copy_buffer.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengl.*
+import org.lwjgl.system.linux.*
+
+val GLX_NV_copy_buffer = "GLXNVCopyBuffer".nativeClassGLX("GLX_NV_copy_buffer", postfix = NV) {
+	nativeImport (
+		"<GL/glx.h>"
+	)
+
+	documentation =
+		"""
+		Native bindings to the ${registryLink("NV", "glx_copy_buffer")} extension.
+
+		Extends ${ARB_copy_buffer.link} to have GLX bindings.
+		"""
+
+	void(
+		"CopyBufferSubDataNV",
+		"",
+
+		DISPLAY,
+		GLXContext.IN("readCtx", ""),
+		GLXContext.IN("writeCtx", ""),
+		GLenum.IN("readTarget", ""),
+		GLenum.IN("writeTarget", ""),
+		GLintptr.IN("readOffset", ""),
+		GLintptr.IN("writeOffset", ""),
+		GLsizeiptr.IN("size", "")
+	)
+
+	void(
+		"NamedCopyBufferSubDataNV",
+		"",
+
+		DISPLAY,
+		GLXContext.IN("readCtx", ""),
+		GLXContext.IN("writeCtx", ""),
+		GLuint.IN("readBuffer", ""),
+		GLuint.IN("writeBuffer", ""),
+		GLintptr.IN("readOffset", ""),
+		GLintptr.IN("writeOffset", ""),
+		GLsizeiptr.IN("size", "")
+	)
+}
---
+++ modules/templates/src/main/kotlin/org/lwjgl/opengl/templates/GLX_NV_swap_group.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengl.*
+import org.lwjgl.system.linux.*
+
+val GLX_NV_swap_group = "GLXNVSwapGroup".nativeClassGLX("GLX_NV_swap_group", postfix = NV) {
+	nativeImport (
+		"<GL/glx.h>"
+	)
+
+	documentation =
+		"""
+		Native bindings to the ${registryLink("NV", "glx_swap_group")} extension.
+
+		This extension provides the capability to synchronize the buffer swaps of a group of OpenGL windows. A swap group is created, and windows are added as
+		members to the swap group. Buffer swaps to members of the swap group will then take place concurrently.
+
+		This extension also provides the capability to sychronize the buffer swaps of different swap groups, which may reside on distributed systems on a
+		network. For this purpose swap groups can be bound to a swap barrier.
+
+		This extension extends the set of conditions that must be met before a buffer swap can take place.
+		"""
+
+	Bool(
+		"JoinSwapGroupNV",
+		"",
+
+		DISPLAY,
+		GLXDrawable.IN("drawable", ""),
+		GLuint.IN("group", "")
+	)
+
+	Bool(
+		"BindSwapBarrierNV",
+		"",
+
+		DISPLAY,
+		GLuint.IN("group", ""),
+		GLuint.IN("barrier", "")
+	)
+
+	Bool(
+		"QuerySwapGroupNV",
+		"",
+
+		DISPLAY,
+		GLXDrawable.IN("drawable", ""),
+		Check(1) _ GLuint_p.OUT("group", ""),
+		Check(1) _ GLuint_p.OUT("barrier", "")
+	)
+
+	Bool(
+		"QueryMaxSwapGroupsNV",
+		"",
+
+		DISPLAY,
+		int.IN("screen", ""),
+		Check(1) _ GLuint_p.OUT("maxGroups", ""),
+		Check(1) _ GLuint_p.OUT("maxBarriers", "")
+	)
+
+	Bool(
+		"QueryFrameCountNV",
+		"",
+
+		DISPLAY,
+		int.IN("screen", ""),
+		Check(1) _ GLuint_p.OUT("count", "")
+	)
+
+	Bool(
+		"ResetFrameCountNV",
+		"",
+
+		DISPLAY,
+		int.IN("screen", "")
+	)
+}
