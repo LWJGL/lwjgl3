4e0683a60a4539dc77bed9119fb897f235d22ee2
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-07-29 19:58:10 +0300

    Add CGL pbuffer functions

--- modules/templates/src/main/kotlin/org/lwjgl/opengl/templates/CGL.kt
+++ modules/templates/src/main/kotlin/org/lwjgl/opengl/templates/CGL.kt
@@ -530,129 +530,352 @@ val CGL = "CGL".nativeClass(OPENGL_PACKAGE, nativeSubPath = "cgl", prefix = "CGL
 		CGLContextObj.IN("ctx", "the CGL rendering context whose format you want to receive")
 	)
 
-	/*// PBuffer functions
+	// PBuffer functions
 
 	CGLError(
 		"CreatePBuffer",
-		"reates a pixel buffer of the specified size, compatible with the specified texture target.",
+		"""
+		Creates a pixel buffer of the specified size, compatible with the specified texture target.
+
+		This function does not have any knowledge of OpenGL contexts or pixel format objects and does not specifically allocate the storage needed for the
+		actual pixel buffer. These operations occur when you call the function #SetPBuffer().
+
+		You can determine the dimensional limits of a pixel buffer by calling the OpenGL function glGetInteger. You can find the maximum size supported by
+		querying GL_MAX_VIEWPORT_DIMS and the minimum size by querying GL_MIN_PBUFFER_VIEWPORT_DIMS_APPLE, which returns two integer values (similar to
+		GL_MAX_VIEWPORT_DIMS). All pixel buffer dimensions that you request with the function aglCreatePBuffer should fall within these limits (inclusively)
+		and should comply with any limitations imposed by the texture target you select.
+
+		The maximum viewport size supported in OS X is quite large. You should take into consideration the amount of video or system memory required to support
+		the requested pixel buffer size, including additional memory needed for multiple buffers and options such as multisampling.
+
+		Starting in OS X v10.5, pixel buffer objects are reference counted. Pixel buffer objects are created with a reference count of 1 and are destroyed when
+		the last reference to the object is released.
+
+		Deprecated in OS X v10.7.
+		""",
 
 		GLsizei.IN("width", "the width, in pixels, of the pixel buffer"),
 		GLsizei.IN("height", "the height, in pixels, of the pixel buffer"),
-		GLenum.IN("target", ""),
-		GLenum.IN("internalFormat", ""),
-		GLint.IN("max_level", ""),
-		CGLPBufferObj_p.IN("pbuffer", "")
+		GLenum.IN(
+			"target",
+			"a constant that specifies the type of the pixel buffer target texture", "GL11#TEXTURE_2D GL13#TEXTURE_CUBE_MAP GL31#TEXTURE_RECTANGLE"
+		),
+		GLenum.IN(
+			"internalFormat",
+			"""
+			a constant that specifies the internal color format of the pixel buffer. The format controls whether the alpha channel of the pixel buffer is used
+			for texturing operations.
+			""",
+		    "GL11#RGB GL11#RGBA"
+		),
+		GLint.IN(
+			"max_level",
+			"""
+			the maximum level of mipmap detail allowable. Pass 0 for a pixel buffer that is not using mipmaps. The value passed should never exceed the actual
+			maximum number of mipmap levels that can be represented with the given width and height.
+			"""
+		),
+		Check(1) _ CGLPBufferObj_p.OUT("pbuffer", "on return, points to a new pixel buffer object")
 	)
 
 	CGLError(
 		"DestroyPBuffer",
-		"",
+		"""
+		Releases the resources associated with a pixel buffer object.
 
-		CGLPBufferObj.IN("pbuffer", "")
+		Starting in OS X v10.5, pixel buffer objects are reference counted. Calling this function is equivalent to calling #ReleasePBuffer().
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLPBufferObj.IN("pbuffer", "the pixel buffer object whose resources you want to release")
 	)
 
 	CGLError(
 		"DescribePBuffer",
-		"",
+		"""
+		Retrieves information that describes the specified pixel buffer object.
 
-		CGLPBufferObj.IN("obj", ""),
-		Check(1) _ GLsizei_p.OUT("width", ""),
-		Check(1) _ GLsizei_p.OUT("height", ""),
-		Check(1) _ GLenum_p.OUT("target", ""),
-		Check(1) _ GLenum_p.OUT("internalFormat", ""),
-		Check(1) _ GLint_p.OUT("mipmap", "")
+		The width, height, texture target, and internal texture color format of a pixel buffer object are set at its creation and cannot be changed without
+		destroying and recreating the object. The level is set when the pixel buffer object is attached to a rendering context by calling the function
+		#SetPBuffer().
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLPBufferObj.IN("obj", "a pointer to the pixel buffer object"),
+		Check(1) _ GLsizei_p.OUT("width", "on return, points to the width, in pixels, of the pixel buffer"),
+		Check(1) _ GLsizei_p.OUT("height", "on return, points to the height, in pixels, of the pixel buffer"),
+		Check(1) _ GLenum_p.OUT("target", "on return, points to a constant that specifies the pixel buffer texture target"),
+		Check(1) _ GLenum_p.OUT("internalFormat", "on return, points to a constant that specifies the internal color format of the pixel buffer"),
+		Check(1) _ GLint_p.OUT("mipmap", "on return, points to the mipmap level of the pixel buffer or 0 if it doesn't use mipmaps")
 	)
 
 	CGLError(
 		"TexImagePBuffer",
-		"",
+		"""
+		Binds the contents of a pixel buffer to a data source for a texture object.
 
-		CGLContextObj.IN("ctx", ""),
-		CGLPBufferObj.IN("pbuffer", ""),
-		GLenum.IN("source", "")
+		You must generate and bind a texture name (using standard OpenGL texturing calls) that is compatible with the pixel buffer texture target. Don't supply
+		a texture object that was used previously for nonpixel buffer texturing operations unless you first call glDeleteTextures to regenerate the texture
+		name.
+
+		If you modify the content of a pixel buffer that uses mipmap levels, you must call this function again before drawing with the pixel buffer, to ensure
+		that the content is synchronized with OpenGL. For pixel buffers without mipmaps, simply rebind to the texture object to synchronize content.
+
+		No OpenGL texturing calls that modify a pixel buffer texture content are permitted (such as glTexSubImage2D or glCopyTexImage2D) with the pixel buffer
+		texture as the destination. It is permitted to use texturing commands to read data from a pixel buffer texture, such as glCopyTexImage2D, with the
+		pixel buffer texture as the source. It is also legal to use OpenGL functions such as glReadPixels to read the contents of a pixel buffer directly
+		through the pixel buffer context.
+
+		Note that texturing with the CGLTexImagePBuffer function can fail to produce the intended results without error in the same way other OpenGL texturing
+		commands can normally fail. The function fails if you set an incompatible filter mode, do not enable the proper texture target, or other conditions
+		described in the OpenGL specification.
+
+		You don't need to share a context to use a pixel buffer object as a texture source. You can use independent pixel format objects and OpenGL contexts
+		for both the pixel buffer and the target drawable object without sharing resources, and still texture using a pixel buffer in the target context.
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLContextObj.IN(
+			"ctx",
+			"""
+			a rendering context, which is the target context for the texture operation. This is the context that you plan to render content to. This is not the
+			context attached to the pixel buffer.
+			"""
+		),
+		CGLPBufferObj.IN("pbuffer", "a pixel buffer object"),
+		GLenum.IN(
+			"source",
+			"""
+			the source buffer to get the texture from, which should be a valid OpenGL buffer such as GL_FRONT or GL_BACK and should be compatible with the
+			buffer and renderer attributes that you used to create the rendering context attached to the pixel buffer. This means that the pixel buffer must
+			possess the buffer in question for the texturing operation to succeed.
+			"""
+		)
 	)
 
 	CGLPBufferObj(
 		"RetainPBuffer",
-		"",
+		"""
+		Increments the retain count on a pixel buffer object.
 
-		CGLPBufferObj.IN("pbuffer", "")
+		Each call to CGLRetainPBuffer increases the retain count by 1. To prevent the pixel buffer object from being leaked, each retain call must be matched
+		with a call to #ReleasePBuffer().
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLPBufferObj.IN("pbuffer", "the pixel buffer object whose retain count you wish to increment")
 	)
 
 	void(
 		"ReleasePBuffer",
-		"",
+		"""
+		Releases the resources associated with a pixel buffer object.
 
-		CGLPBufferObj.IN("pbuffer", "")
+		Starting in OS X v10.5, pixel buffer objects are reference counted. Calling this function is equivalent to calling CGLReleasePBuffer.
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLPBufferObj.IN("pbuffer", "the pixel buffer object whose resources you want to release")
 	)
 
 	GLuint(
 		"GetPBufferRetainCount",
-		"",
+		"""
+		Returns the retain count of a pixel buffer object.
 
-		CGLPBufferObj.IN("pbuffer", "")
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLPBufferObj.IN("pbuffer", "the pixel buffer object whose retain count you wish to retrieve")
 	)
 
 	// Drawable Functions
 
 	CGLError(
 		"SetOffScreen",
-		"",
+		"""
+		Attaches a rendering context to an offscreen buffer.
 
-		CGLContextObj.IN("ctx", ""),
-		GLsizei.IN("width", ""),
-		GLsizei.IN("height", ""),
-		GLint.IN("rowbytes", ""),
-		voidptr.IN("baseaddr", "")
+		Before calling this function, you must set up the rendering context using a pixel format object created with the kCGLPFAOffScreen attribute. For more
+		information about kCGLPFAOffScreen, see Buffer and Renderer Attributes.
+
+		After calling this function, subsequent OpenGL drawing is rendered into the offscreen buffer and the viewport of the rendering context is set to the
+		full size of the offscreen area.
+
+		To exit offscreen mode, call #ClearDrawable().
+
+		To obtain functionality similar to offscreen mode on renderers that do not support it, attach the context to a hidden window and use the OpenGL
+		function glReadPixels.
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLContextObj.IN("ctx", "a rendering context"),
+		GLsizei.IN("width", "the width, in pixels, of the offscreen buffer"),
+		GLsizei.IN("height", "the height, in pixels, of the offscreen buffer"),
+		GLint.IN("rowbytes", "the number of bytes per row of the offscreen buffer, which must be greater than or equal to width times bytes per pixel"),
+		Check("rowbytes * height") _ void_p.IN(
+			"baseaddr",
+			"a pointer to a block of memory to use as the offscreen buffer. The size of the memory must be at least {@code rowbytes*height} bytes."
+		)
 	)
 
 	CGLError(
 		"GetOffScreen",
-		"",
+		"""
+		Retrieves an offscreen buffer and its parameters for a specified rendering context.
 
-		CGLContextObj.IN("ctx", ""),
-		Check(1) _ GLsizei_p.OUT("width", ""),
-		Check(1) _ GLsizei_p.OUT("height", ""),
-		Check(1) _ GLint_p.OUT("rowbytes", ""),
-		Check(1) _ voidptr_p.OUT("baseaddr", "")
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLContextObj.IN("ctx", "a rendering context"),
+		Check(1) _ GLsizei_p.OUT(
+			"width",
+			"""
+			on return, points to the width, in pixels, of the offscreen buffer. If the rendering context is not attached to an offscreen drawable object, the
+			value of width is set to 0.
+			"""
+		),
+		Check(1) _ GLsizei_p.OUT(
+			"height",
+			"""
+			on return, points to the height, in pixels, of the offscreen buffer. If the rendering context is not attached to an offscreen drawable object, the
+			value of height is set to 0.
+			"""
+		),
+		Check(1) _ GLint_p.OUT(
+			"rowbytes",
+			"""
+			on return, points to the number of bytes per row of the offscreen buffer. If the context is not attached to an offscreen drawable object, the value
+			of rowbytes is set to 0.
+			"""
+		),
+		Check(1) _ voidptr_p.OUT(
+			"baseaddr",
+			"""
+			on return, points to the base address of the offscreen buffer. If the context is not attached to an offscreen drawable object, the value of
+			{@code baseaddr} is set to $NULL.
+			"""
+		)
 	)
 
 	CGLError(
 		"SetFullScreen",
-		"",
+		"""
+		Attaches a rendering context to its full-screen drawable object.
 
-		CGLContextObj.IN("ctx", "")
+		Before calling this function, you must set up the rendering context using a pixel format object created with the kCGLPFAFullScreen attribute (see
+		Buffer and Renderer Attributes). Some OpenGL renderers, such as the software renderer, do not support full-screen mode. After you call the function
+		#ChoosePixelFormat() with the full-screen attribute, you need to check whether the pixel format object is created successfully.
+
+		You must capture the display prior to entering full-screen mode and release it after exiting. After calling this function, subsequent OpenGL drawing is
+		rendered into the entire screen. For more information, see
+		<a href="https://developer.apple.com/library/mac/documentation/GraphicsImaging/Conceptual/OpenGL-MacProgGuide/opengl_intro/opengl_intro.html\#//apple_ref/doc/uid/TP40001987">OpenGL Programming Guide for Mac</a>.
+
+		To exit full-screen mode, call #ClearDrawable().
+
+		Deprecated in OS X v10.7. Use #SetFullScreenOnDisplay() instead.
+		""",
+
+		CGLContextObj.IN("ctx", "a rendering context")
 	)
 
 	CGLError(
 		"SetFullScreenOnDisplay",
-		"",
+		"""
+		Attaches a rendering context to a full-screen drawable object." +
 
-		CGLContextObj.IN("ctx", ""),
-		GLuint.IN("display_mask", "")
+		This function obtains a drawable object that covers an entire screen and attaches it to the rendering context. A full-screen rendering context may
+		allow the underlying renderer to provide better performance compared to a context associated with a window that partially covers the screen.
+
+		Prior to calling this function, your application should ensure that the context is capable of rendering to this display by querying the appropriate
+		renderer properties. For more information, see #QueryRendererInfo(). Note that some renderers, including the software renderer, do not support
+		full-screen mode.
+
+		You must capture the screen prior to entering full-screen mode and release it after exiting. After calling this function, subsequent OpenGL drawing is
+		rendered into the entire screen. For more information, see OpenGL Programming Guide for Mac.
+
+		To exit full-screen mode, call #ClearDrawable().
+
+		In OS X v10.6 or later, this function is not deprecated, but is usually not necessary. If your application creates a window that completely covers the
+		screen, the system implicitly creates a full-screen instance, for the same potential performance benefit.
+
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLContextObj.IN("ctx", "a rendering context"),
+		GLuint.IN("display_mask", "a bit field that contains the OpenGL display mask for the screen you wish the context to cover")
 	)
 
 	CGLError(
 		"SetPBuffer",
-		"",
+		"""
+		Attaches a pixel buffer object to a rendering context.
 
-		CGLContextObj.IN("ctx", ""),
-		CGLPBufferObj.IN("pbuffer", ""),
-		GLenum.IN("face", ""),
-		GLint.IN("level", ""),
-		GLint.IN("screen", "")
+		The first time you call this function for a specific pixel buffer object, the system creates the necessary buffers. The buffers are created to support
+		the attributes dictated by the pixel format object used to create the rendering context and by the parameters used to create the pixel buffer object.
+		The storage requirements for pixel buffer objects, which can be quite large, are very similar to the requirements for windows or views with OpenGL
+		contexts attached. All drawable objects compete for the same scarce resources. This function can fail is there is not enough contiguous VRAM for each
+		buffer. It's best to code defensively with a scheme that reduces resource consumption without causing the application to resort to failure. Unless, of
+		course, failure is the only viable alternative.
+
+		The ability to attach a pixel buffer to a context is supported only on renderers that export GL_APPLE_pixel_buffer in the GL_EXTENSIONS string. Before
+		calling this function, you should programmatically determine if it’s possible to attach a pixel buffer to a context by querying GL_EXTENSIONS in the
+		context and looking for GL_APPLE_pixel_buffer. If that extension is not present, the renderer won’t allow setting the pixel buffer.
+
+		In order of performance, these are the renderers you should consider using when setting up a rendering context to attach to a pixel buffer:
+		${ul(
+			"A hardware renderer.",
+		    "The generic render, but only with an offscreen pixel format and glTexSubImage.",
+		    "The Apple software renderer, which supports pixel buffers in OS X v10.4.8 and later."
+		)}
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLContextObj.IN("ctx", "the rendering context to attach the pixel buffer to"),
+		CGLPBufferObj.IN("pbuffer", "a pixel buffer object"),
+		GLenum.IN("face", "the cube map face to draw if the pixel buffer texture target type is GL13#TEXTURE_CUBE_MAP; otherwise pass 0."),
+		GLint.IN(
+			"level",
+			"""
+			the mipmap level to draw. This must not exceed the maximum mipmap level set when the pixel buffer object was created. Pass 0 for a texture target
+			that does not support mipmaps.
+			"""),
+		GLint.IN(
+			"screen",
+			"""
+			a virtual screen value. The virtual screen determines the renderer OpenGL uses to draw to the pixel buffer object. For best performance, for a
+			pixel buffer used as a texture source, you should supply the virtual screen value that results in using the same renderer used by the context
+			that's the texturing target.
+			"""
+		)
 	)
 
 	CGLError(
 		"GetPBuffer",
-		"",
+		"""
+		Retrieves a pixel buffer and its parameters for a specified rendering context.
 
-		CGLContextObj.IN("ctx", ""),
-		Check(1) _ CGLPBufferObj_p.OUT("pbuffer", ""),
-		Check(1) _ GLenum_p.OUT("face", ""),
-		Check(1) _ GLint_p.OUT("level", ""),
-		Check(1) _ GLint_p.OUT("screen", "")
-	)*/
+		Deprecated in OS X v10.7.
+		""",
+
+		CGLContextObj.IN("ctx", "a rendering context"),
+		Check(1) _ CGLPBufferObj_p.OUT("pbuffer", "on return, points to the pixel buffer object attached to the rendering context"),
+		Check(1) _ GLenum_p.OUT(
+			"face",
+			"""
+			on return, points to the cube map face that is set if the pixel buffer texture target type is GL13#TEXTURE_CUBE_MAP; otherwise 0 for all other
+			texture target types.
+			"""
+		),
+		Check(1) _ GLint_p.OUT("level", "on return, points to the current mipmap level for drawing"),
+		Check(1) _ GLint_p.OUT("screen", "on return, points to the current virtual screen number, as set by the last valid call to #SetPBuffer()")
+	)
 
 	CGLError(
 		"ClearDrawable",
@@ -769,7 +992,7 @@ val CGL = "CGL".nativeClass(OPENGL_PACKAGE, nativeSubPath = "cgl", prefix = "CGL
 
 	CGLError(
 		"UpdateContext",
-		"",
+		"Synchronizes new renderer state to that of the application context",
 
 		CGLContextObj.IN("ctx", "a rendering context")
 	)
