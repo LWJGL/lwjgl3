f0e45e3332127c9e8df8a8688aeb5c9587b1de9c
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-08-05 19:16:04 +0300

    Only generate if EGL is enabled

--- modules/templates/src/main/kotlin/org/lwjgl/glfw/templates/GLFWEGL.kt
+++ modules/templates/src/main/kotlin/org/lwjgl/glfw/templates/GLFWEGL.kt
@@ -8,8 +8,9 @@ import org.lwjgl.generator.*
 import org.lwjgl.glfw.*
 import org.lwjgl.egl.*
 
-val GLFWEGL = "GLFWEGL".nativeClass(packageName = GLFW_PACKAGE, nativeSubPath = "egl", prefix = "GLFW") {
-	nativeDirective(
+val GLFWEGL = dependsOn(Binding.EGL) {
+	"GLFWEGL".nativeClass(packageName = GLFW_PACKAGE, nativeSubPath = "egl", prefix = "GLFW") {
+		nativeDirective(
 """#ifdef LWJGL_WINDOWS
 	#define GLFW_EXPOSE_NATIVE_WIN32
 #endif
@@ -17,53 +18,54 @@ val GLFWEGL = "GLFWEGL".nativeClass(packageName = GLFW_PACKAGE, nativeSubPath = 
 	#define GLFW_EXPOSE_NATIVE_X11
 #endif
 #define GLFW_EXPOSE_NATIVE_EGL"""
-	)
+		)
 
-	nativeImport(
-		"glfw3.h",
-		"glfw3native.h"
-	)
+		nativeImport(
+			"glfw3.h",
+			"glfw3native.h"
+		)
 
-	javaImport(
-		"org.lwjgl.egl.EGL10"
-	)
+		javaImport(
+			"org.lwjgl.egl.EGL10"
+		)
 
-	documentation = "Native bindings to the GLFW library's EGL native access functions."
+		documentation = "Native bindings to the GLFW library's EGL native access functions."
 
-	EGLDisplay(
-		"GetEGLDisplay",
-		"""
-		Returns the {@code EGLDisplay} used by GLFW.
+		EGLDisplay(
+			"GetEGLDisplay",
+			"""
+			Returns the {@code EGLDisplay} used by GLFW.
 
-		This function may be called from any thread. Access is not synchronized.
-		""",
+			This function may be called from any thread. Access is not synchronized.
+			""",
 
-		returnDoc = "the {@code EGLDisplay} used by GLFW, or EGL10##EGL_NO_DISPLAY if an error occured"
-	)
+			returnDoc = "the {@code EGLDisplay} used by GLFW, or EGL10##EGL_NO_DISPLAY if an error occured"
+		)
 
-	EGLContext(
-		"GetEGLContext",
-		"""
-		Returns the {@code EGLContext} of the specified window.
+		EGLContext(
+			"GetEGLContext",
+			"""
+			Returns the {@code EGLContext} of the specified window.
 
-		This function may be called from any thread. Access is not synchronized.
-		""",
+			This function may be called from any thread. Access is not synchronized.
+			""",
 
-		GLFWwindow.IN("window", "a GLFW window"),
+			GLFWwindow.IN("window", "a GLFW window"),
 
-	    returnDoc = "the {@code EGLContext} of the specified window, or EGL10##EGL_NO_CONTEXT if an error occurred"
-	)
+			returnDoc = "the {@code EGLContext} of the specified window, or EGL10##EGL_NO_CONTEXT if an error occurred"
+		)
 
-	EGLSurface(
-		"GetEGLSurface",
-		"""
-		Returns the {@code EGLSurface} of the specified window.
+		EGLSurface(
+			"GetEGLSurface",
+			"""
+			Returns the {@code EGLSurface} of the specified window.
 
-		This function may be called from any thread. Access is not synchronized.
-		""",
+			This function may be called from any thread. Access is not synchronized.
+			""",
 
-		GLFWwindow.IN("window", ""),
+			GLFWwindow.IN("window", ""),
 
-		returnDoc = "the {@code EGLSurface} of the specified window, or EGL10##EGL_NO_SURFACE if an error occurred"
-	)
+			returnDoc = "the {@code EGLSurface} of the specified window, or EGL10##EGL_NO_SURFACE if an error occurred"
+		)
+	}
 }
