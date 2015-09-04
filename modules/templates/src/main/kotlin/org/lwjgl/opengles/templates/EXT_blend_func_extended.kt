82899b723bcef0c4afc715f632043718b9df90c1
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-09-04 19:13:55 +0300

    [GLES] Add EXT_blend_func_extended and EXT_multisample_compatibility

---
+++ modules/templates/src/main/kotlin/org/lwjgl/opengles/templates/EXT_blend_func_extended.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengles.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengles.*
+
+val EXT_blend_func_extended = "EXTBlendFuncExtended".nativeClassGLES("EXT_blend_func_extended", postfix = EXT) {
+	documentation =
+		"""
+		Native bindings to the $registryLink extension.
+
+		This extension provides an ES version of the ARB_blend_func_extended functionality.
+
+		Traditional OpenGL includes fixed-function blending that combines source colors with the existing content of a render buffer in a variety of ways. A
+		number of extensions have enhanced this functionality by adding further sources of blending weights and methods to combine them. However, the inputs to
+		the fixed-function blending units are constrained to a source color (as output from fragment shading), destination color (as the current content of the
+		frame buffer) or constants that may be used in their place.
+
+		This extension adds new blending functions whereby a fragment shader may output two colors, one of which is treated as the source color, and the other
+		used as a blending factor for either source or destination colors. Furthermore, this extension increases orthogonality by allowing the
+		SRC_ALPHA_SATURATE function to be used as the destination weight.
+
+		Because of the limitations of the OpenGL ES 2.0 shading language, new built-in variables (gl_SecondaryFragColorEXT, gl_SecondaryFragDataEXT) are added
+		to the ES 1.00 shading language rather than introduce more complex features for user-defined fragment outputs. Because such built-in variable are
+		deprecated in ES 3.0, these variables are NOT available in the OpenGL ES 3.xx shading language verisons.
+		"""
+
+	IntConstant(
+		"""
+		Accepted by the {@code src} and {@code dst} parameters of BlendFunc and BlendFunciEXT, and by the {@code srcRGB}, {@code dstRGB}, {@code srcAlpha} and
+		{@code dstAlpha} parameters of BlendFuncSeparate and BlendFuncSeparateiEXT.
+		""",
+
+		"SRC1_COLOR_EXT" _ 0x88F9,
+		"SRC1_ALPHA_EXT" _ 0x8589,
+		"ONE_MINUS_SRC1_COLOR_EXT" _ 0x88FA,
+		"ONE_MINUS_SRC1_ALPHA_EXT" _ 0x88FB,
+		"SRC_ALPHA_SATURATE_EXT" _ 0x0308
+	)
+
+	IntConstant(
+		"Accepted in the {@code props} array of GetProgramResourceiv.",
+
+		"LOCATION_INDEX_EXT" _ 0x930F
+	)
+
+	IntConstant(
+		"Accepted by the {@code pname} parameter of GetBooleanv, GetIntegerv, and GetFloatv.",
+
+		"MAX_DUAL_SOURCE_DRAW_BUFFERS_EXT" _ 0x88FC
+	)
+
+	void(
+		"BindFragDataLocationIndexedEXT",
+		"",
+
+		GLuint.IN("program", ""),
+		GLuint.IN("colorNumber", ""),
+		GLuint.IN("index", ""),
+		const _ GLcharASCII_p.IN("name", "")
+	)
+
+	GLint(
+		"GetFragDataIndexEXT",
+		"",
+
+		GLuint.IN("program", ""),
+		const _ GLcharASCII_p.IN("name", "")
+	)
+
+	void(
+		"BindFragDataLocationEXT",
+		"",
+
+		GLuint.IN("program", ""),
+		GLuint.IN("colorNumber", ""),
+		const _ GLcharASCII_p.IN("name", "")
+	)
+
+	GLint(
+		"GetProgramResourceLocationIndexEXT",
+		"",
+
+		GLuint.IN("program", ""),
+		GLenum.IN("programInterface", ""),
+		const _ GLcharASCII_p.IN("name", "")
+	)
+}
---
+++ modules/templates/src/main/kotlin/org/lwjgl/opengles/templates/EXT_multisample_compatibility.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.opengles.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opengles.*
+
+val EXT_multisample_compatibility = "EXTMultisampleCompatibility".nativeClassGLES("EXT_multisample_compatibility", postfix = EXT) {
+	documentation =
+		"""
+		Native bindings to the $registryLink extension.
+
+		This extension allows disabling and enabling of multisampling.
+
+		OpenGL ES 2.x/3.x both support multisampling but provide no way to disable multisampling for drawables with multiple samples.
+
+		This introduces the MULTISAMPLE and SAMPLE_ALPHA_TO_ONE enables (though EXT suffixed) that operate identically to the OpenGL 4.5 core profile (as they
+		have existed in OpenGL since OpenGL 1.3 and earlier with the ARB_multisample, EXT_multisample, and SGIS_multisample extensions).
+		"""
+
+	IntConstant(
+		"""
+		Accepted by the {@code cap} parameter to Enable, Disable, and IsEnabled and the {@code pname} parameter to GetBooleanv, GetIntegerv, GetFloatv, and
+		GetInteger64v.
+		""",
+
+		"MULTISAMPLE_EXT" _ 0x809D,
+		"SAMPLE_ALPHA_TO_ONE_EXT" _ 0x809F
+	)
+}
