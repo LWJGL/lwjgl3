d7f96d73d1ce1a400e2fe9e76f194297678422d1
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-12-09 01:33:10 +0200

    Disable more features

--- modules/core/src/main/include/system/windows/WindowsLWJGL.h
+++ modules/core/src/main/include/system/windows/WindowsLWJGL.h
@@ -37,9 +37,9 @@
 
 	#define NOGDICAPMASKS // CC_*, LC_*, PC_*, CP_*, TC_*, RC_
 	#define NOVIRTUALKEYCODES // VK_*
-	//#define NOWINMESSAGES // WM_*, EM_*, LB_*, CB_*
+	#define NOWINMESSAGES // WM_*, EM_*, LB_*, CB_*
 	#define NOWINSTYLES // WS_*, CS_*, ES_*, LBS_*, SBS_*, CBS_*
-	//#define NOSYSMETRICS // SM_*
+	#define NOSYSMETRICS // SM_*
 	#define NOMENUS // MF_*
 	#define NOICONS // IDI_*
 	#define NOKEYSTATES // MK_*
@@ -48,26 +48,26 @@
 	#define NOSHOWWINDOW // SW_*
 	#define OEMRESOURCE // OEM Resource values
 	#define NOATOM // Atom Manager routines
-	//#define NOCLIPBOARD // Clipboard routines
+	#define NOCLIPBOARD // Clipboard routines
 	#define NOCOLOR // Screen colors
 	#define NOCTLMGR // Control and Dialog routines
 	#define NODRAWTEXT // DrawText() and DT_*
-	//#define NOGDI // All GDI defines and routines
+	#define NOGDI // All GDI defines and routines
 	#define NOKERNEL // All KERNEL defines and routines
-	//#define NOUSER // All USER defines and routines
+	#define NOUSER // All USER defines and routines
 	#define NONLS // All NLS defines and routines
 	#define NOMB // MB_* and MessageBox()
 	#define NOMEMMGR // GMEM_*, LMEM_*, GHND, LHND, associated routines
 	#define NOMETAFILE // typedef METAFILEPICT
 	#define NOMINMAX // Macros min(a,b) and max(a,b)
-	//#define NOMSG // typedef MSG and associated routines
+	#define NOMSG // typedef MSG and associated routines
 	#define NOOPENFILE // OpenFile(), OemToAnsi, AnsiToOem, and OF_*
 	#define NOSCROLL // SB_* and scrolling routines
 	#define NOSERVICE // All Service Controller routines, SERVICE_ equates, etc.
 	#define NOSOUND // Sound driver routines
 	#define NOTEXTMETRIC // typedef TEXTMETRIC and associated routines
 	#define NOWH // SetWindowsHook and WH_*
-	//#define NOWINOFFSETS // GWL_*, GCL_*, associated routines
+	#define NOWINOFFSETS // GWL_*, GCL_*, associated routines
 	#define NOCOMM // COMM driver routines
 	#define NOKANJI // Kanji support stuff.
 	#define NOHELP // Help engine interface.
@@ -79,6 +79,5 @@
 
     DISABLE_WARNINGS()
 	#include <windows.h>
-	#include <mmsystem.h>
 	ENABLE_WARNINGS()
 #endif /* _WINDOWS_LWJGL_INCLUDED */
