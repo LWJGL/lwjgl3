dfe41694e58683080b5dc2ec575511f7278e48e3
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-01-02 19:02:12 +0200

    NativeFileDialog fixes

---
+++ modules/core/src/test/java/org/lwjgl/demo/nfd/HelloNFD.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ */
+package org.lwjgl.demo.nfd;
+
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.nfd.NFDPathSet;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.nfd.NativeFileDialog.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public final class HelloNFD {
+
+	private HelloNFD() {
+	}
+
+	public static void main(String[] args) {
+		GLFWErrorCallback errorfun = GLFWErrorCallback.createPrint();
+		GLFWKeyCallback keyfun = new GLFWKeyCallback() {
+			@Override
+			public void invoke(long window, int key, int scancode, int action, int mods) {
+				if ( action == GLFW_RELEASE )
+					return;
+
+				switch ( key ) {
+					case GLFW_KEY_ESCAPE:
+						glfwSetWindowShouldClose(window, GLFW_TRUE);
+						break;
+					case GLFW_KEY_O:
+						if ( (mods & GLFW_MOD_CONTROL) != 0 ) {
+							if ( (mods & GLFW_MOD_SHIFT) != 0 )
+								openMulti();
+							else
+								openSingle();
+						}
+						break;
+					case GLFW_KEY_S:
+						if ( (mods & GLFW_MOD_CONTROL) != 0 )
+							save();
+						break;
+				}
+			}
+		};
+
+		errorfun.set();
+		if ( glfwInit() != GLFW_TRUE )
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		long window = glfwCreateWindow(300, 300, "Hello NativeFileDialog!", NULL, NULL);
+		if ( window == NULL )
+			throw new RuntimeException("Failed to create the GLFW window");
+
+		keyfun.set(window);
+
+		// Center window
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(
+			window,
+			(vidmode.width() - 300) / 2,
+			(vidmode.height() - 300) / 2
+		);
+
+		glfwMakeContextCurrent(window);
+		GL.createCapabilities();
+
+		glfwSwapInterval(1);
+
+		System.out.println("Press Ctrl+O to launch the single file open dialog.");
+		System.out.println("Press Ctrl+Shift+O to launch the multi file open dialog.");
+		System.out.println("Press Ctrl+S to launch the file save dialog.");
+		while ( glfwWindowShouldClose(window) == GLFW_FALSE ) {
+			glfwPollEvents();
+
+			glClear(GL_COLOR_BUFFER_BIT);
+			glfwSwapBuffers(window);
+		}
+
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+
+		keyfun.release();
+		errorfun.release();
+	}
+
+	private static void openSingle() {
+		PointerBuffer outPath = memAllocPointer(1);
+
+		try {
+			int result = NFD_OpenDialog("png,jpg;pdf", null, outPath);
+			switch ( result ) {
+				case NFD_OKAY:
+					System.out.println("Success!");
+					System.out.println(outPath.getStringUTF8(0));
+					nNFD_Free(outPath.get(0));
+					break;
+				case NFD_CANCEL:
+					System.out.println("User pressed cancel.");
+					break;
+				default: // NFD_ERROR
+					System.err.format("Error: %s\n", NFD_GetError());
+			}
+		} finally {
+			memFree(outPath);
+		}
+	}
+
+	private static void openMulti() {
+		NFDPathSet pathSet = NFDPathSet.calloc();
+
+		try {
+			int result = NFD_OpenDialogMultiple("png,jpg;pdf", null, pathSet);
+			switch ( result ) {
+				case NFD_OKAY:
+					long count = NFD_PathSet_GetCount(pathSet);
+					for ( long i = 0; i < count; i++ ) {
+						String path = NFD_PathSet_GetPath(pathSet, i);
+						System.out.format("Path %d: %s\n", i, path);
+					}
+					NFD_PathSet_Free(pathSet);
+					break;
+				case NFD_CANCEL:
+					System.out.println("User pressed cancel.");
+					break;
+				default: // NFD_ERROR
+					System.err.format("Error: %s\n", NFD_GetError());
+			}
+		} finally {
+			pathSet.free();
+		}
+	}
+
+	private static void save() {
+		PointerBuffer savePath = memAllocPointer(1);
+
+		try {
+			int result = NFD_SaveDialog("png,jpg;pdf", null, savePath);
+			switch ( result ) {
+				case NFD_OKAY:
+					System.out.println("Success!");
+					System.out.println(savePath.getStringUTF8(0));
+					nNFD_Free(savePath.get(0));
+					break;
+				case NFD_CANCEL:
+					System.out.println("User pressed cancel.");
+					break;
+				default: // NFD_ERROR
+					System.err.format("Error: %s\n", NFD_GetError());
+			}
+		} finally {
+			memFree(savePath);
+		}
+	}
+
+}
