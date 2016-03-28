LWJGL/lwjgl3

commit 6362edb7d38923200ef4a46f7571420c16f79b38

Add a ByteBuffer version to getFunctionAddress to simplify and optimize function loading

modules/core/src/main/java/org/lwjgl/system/macosx/MacOSXLibraryBundle.java
@@ -41,8 +41,8 @@ public class MacOSXLibraryBundle extends MacOSXLibrary {
 	}
 
 	@Override
-	public long getFunctionAddress(CharSequence functionName) {
-		long nameRef = CString2CFString(memEncodeASCII(functionName, SYSTEM_ALLOCATOR), kCFStringEncodingASCII);
+	public long getFunctionAddress(ByteBuffer functionName) {
+		long nameRef = CString2CFString(functionName, kCFStringEncodingASCII);
 		try {
 			return CFBundleGetFunctionPointerForName(address(), nameRef);
 		} finally {

modules/core/src/main/java/org/lwjgl/egl/EGL.java
@@ -6,6 +6,7 @@ package org.lwjgl.egl;
 
 import org.lwjgl.system.*;
 
+import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -14,7 +15,6 @@ import static java.lang.Math.*;
 import static org.lwjgl.egl.EGL10.*;
 import static org.lwjgl.system.APIUtil.*;
 import static org.lwjgl.system.JNI.*;
-import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 /**
@@ -78,7 +78,7 @@ public final class EGL {
 
 	private static void create(final SharedLibrary EGL) {
 		try {
-			FunctionProvider functionProvider = new FunctionProvider() {
+			FunctionProvider functionProvider = new FunctionProvider.Default() {
 				private final long eglGetProcAddress = EGL.getFunctionAddress("eglGetProcAddress");
 
 				{
@@ -89,20 +89,15 @@ public final class EGL {
 				}
 
 				@Override
-				public long getFunctionAddress(CharSequence functionName) {
-					stackPush();
-					try {
-						long address = invokePP(eglGetProcAddress, memAddress(memEncodeASCII(functionName, true, BufferAllocator.STACK)));
-						if ( address == NULL ) {
-							address = EGL.getFunctionAddress(functionName);
-							if ( address == NULL && Checks.DEBUG_FUNCTIONS )
-								apiLog("Failed to locate address for EGL function " + functionName);
-						}
-
-						return address;
-					} finally {
-						stackPop();
+				public long getFunctionAddress(ByteBuffer functionName) {
+					long address = invokePP(eglGetProcAddress, memAddress(functionName));
+					if ( address == NULL ) {
+						address = EGL.getFunctionAddress(functionName);
+						if ( address == NULL && Checks.DEBUG_FUNCTIONS )
+							apiLog("Failed to locate address for EGL function " + functionName);
 					}
+
+					return address;
 				}
 
 				@Override

modules/core/src/main/java/org/lwjgl/openal/AL.java
@@ -7,6 +7,7 @@ package org.lwjgl.openal;
 import org.lwjgl.system.*;
 
 import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
 import java.util.*;
 
 import static org.lwjgl.openal.AL10.*;
@@ -64,23 +65,18 @@ public final class AL {
 	private AL() {}
 
 	static void init() {
-		functionProvider = new FunctionProvider() {
+		functionProvider = new FunctionProvider.Default() {
 			// We'll use alGetProcAddress for both core and extension entry points.
 			// To do that, we need to first grab the alGetProcAddress function from
 			// the OpenAL native library.
 			private final long alGetProcAddress = ALC.getFunctionProvider().getFunctionAddress("alGetProcAddress");
 
 			@Override
-			public long getFunctionAddress(CharSequence functionName) {
-				stackPush();
-				try {
-					long address = invokePP(alGetProcAddress, memAddress(memEncodeASCII(functionName, true, BufferAllocator.STACK)));
-					if ( address == NULL && Checks.DEBUG_FUNCTIONS )
-						apiLog("Failed to locate address for AL function " + functionName);
-					return address;
-				} finally {
-					stackPop();
-				}
+			public long getFunctionAddress(ByteBuffer functionName) {
+				long address = invokePP(alGetProcAddress, memAddress(functionName));
+				if ( address == NULL && Checks.DEBUG_FUNCTIONS )
+					apiLog("Failed to locate address for AL function " + functionName);
+				return address;
 			}
 
 			@Override

modules/core/src/main/java/org/lwjgl/openal/ALC.java
@@ -6,6 +6,7 @@ package org.lwjgl.openal;
 
 import org.lwjgl.system.*;
 
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.*;
 
@@ -75,7 +76,7 @@ public final class ALC {
 		final SharedLibrary OPENAL = Library.loadNative(libName);
 
 		try {
-			FunctionProviderLocal functionProvider = new FunctionProviderLocal() {
+			FunctionProviderLocal functionProvider = new FunctionProviderLocal.Default() {
 
 				private final long alcGetProcAddress = getFunctionAddress("alcGetProcAddress");
 
@@ -87,7 +88,7 @@ public final class ALC {
 				}
 
 				@Override
-				public long getFunctionAddress(CharSequence functionName) {
+				public long getFunctionAddress(ByteBuffer functionName) {
 					long address = OPENAL.getFunctionAddress(functionName);
 					if ( address == NULL && Checks.DEBUG_FUNCTIONS )
 						apiLog("Failed to locate address for ALC core function " + functionName);
@@ -95,16 +96,11 @@ public final class ALC {
 				}
 
 				@Override
-				public long getFunctionAddress(long handle, CharSequence functionName) {
-					stackPush();
-					try {
-						long address = invokePPP(alcGetProcAddress, handle, memAddress(memEncodeASCII(functionName, true, BufferAllocator.STACK)));
-						if ( address == NULL && Checks.DEBUG_FUNCTIONS )
-							apiLog("Failed to locate address for ALC extension function " + functionName);
-						return address;
-					} finally {
-						stackPop();
-					}
+				public long getFunctionAddress(long handle, ByteBuffer functionName) {
+					long address = invokePPP(alcGetProcAddress, handle, memAddress(functionName));
+					if ( address == NULL && Checks.DEBUG_FUNCTIONS )
+						apiLog("Failed to locate address for ALC extension function " + functionName);
+					return address;
 				}
 
 				@Override

modules/core/src/main/java/org/lwjgl/opencl/CL.java
@@ -73,7 +73,7 @@ public final class CL {
 
 	private static void create(final SharedLibrary OPENCL) {
 		try {
-			FunctionProviderLocal functionProvider = new FunctionProviderLocal() {
+			FunctionProviderLocal functionProvider = new FunctionProviderLocal.Default() {
 
 				private final long clGetExtensionFunctionAddress;
 				private final long clGetExtensionFunctionAddressForPlatform;
@@ -148,36 +148,27 @@ public final class CL {
 				}
 
 				@Override
-				public long getFunctionAddress(CharSequence functionName) {
-					MemoryStack stack = stackPush();
-					try {
-						long nameEncoded = memAddress(memEncodeASCII(functionName, true, BufferAllocator.STACK));
+				public long getFunctionAddress(ByteBuffer functionName) {
+					long nameEncoded = memAddress(functionName);
 
-						long address = platform == NULL
-							? callPP(clGetExtensionFunctionAddress, nameEncoded)
-							: callPPP(clGetExtensionFunctionAddressForPlatform, platform, nameEncoded);
+					long address = platform == NULL
+						? callPP(clGetExtensionFunctionAddress, nameEncoded)
+						: callPPP(clGetExtensionFunctionAddressForPlatform, platform, nameEncoded);
 
-						if ( address == NULL ) {
-							address = OPENCL.getFunctionAddress(functionName);
-							if ( address == NULL && Checks.DEBUG_FUNCTIONS )
-								apiLog("Failed to locate address for CL function " + functionName);
-						}
-
-						return address;
-					} finally {
-						stack.pop();
+					if ( address == NULL ) {
+						address = OPENCL.getFunctionAddress(functionName);
+						if ( address == NULL && Checks.DEBUG_FUNCTIONS )
+							apiLog("Failed to locate address for CL function " + functionName);
 					}
+
+					return address;
 				}
 
 				@Override
-				public long getFunctionAddress(long handle, CharSequence functionName) {
+				public long getFunctionAddress(long handle, ByteBuffer functionName) {
 					MemoryStack stack = stackPush();
 					try {
-						long address = callPPP(
-							clGetExtensionFunctionAddressForPlatform,
-							handle,
-							memAddress(memEncodeASCII(functionName, true, BufferAllocator.STACK))
-						);
+						long address = callPPP(clGetExtensionFunctionAddressForPlatform, handle, memAddress(functionName));
 						if ( address == NULL && Checks.DEBUG_FUNCTIONS )
 							apiLog("Failed to locate address for CL function " + functionName);
 						return address;
@@ -286,9 +277,9 @@ public final class CL {
 		APIVersion version = apiParseVersion(getPlatformInfoStringASCII(cl_platform_id, CL_PLATFORM_VERSION), "OpenCL");
 		CL.addCLVersions(version.major, version.minor, supportedExtensions);
 
-		return new CLCapabilities(new FunctionProvider() {
+		return new CLCapabilities(new FunctionProvider.Default() {
 			@Override
-			public long getFunctionAddress(CharSequence functionName) {
+			public long getFunctionAddress(ByteBuffer functionName) {
 				return getFunctionProvider().getFunctionAddress(cl_platform_id, functionName);
 			}
 			@Override

modules/core/src/main/java/org/lwjgl/opengl/GL.java
@@ -7,6 +7,7 @@ package org.lwjgl.opengl;
 import org.lwjgl.system.*;
 
 import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.*;
 
@@ -104,24 +105,19 @@ public final class GL {
 	}
 
 	private static void create(final SharedLibrary OPENGL) {
-		abstract class FunctionProviderGL implements FunctionProvider {
+		abstract class FunctionProviderGL extends FunctionProvider.Default {
 			abstract long getExtensionAddress(long name);
 
 			@Override
-			public long getFunctionAddress(CharSequence functionName) {
-				stackPush();
-				try {
-					long address = getExtensionAddress(memAddress(memEncodeASCII(functionName, true, BufferAllocator.STACK)));
-					if ( address == NULL ) {
-						address = OPENGL.getFunctionAddress(functionName);
-						if ( address == NULL && DEBUG_FUNCTIONS )
-							apiLog("Failed to locate address for GL function " + functionName);
-					}
-
-					return address;
-				} finally {
-					stackPop();
+			public long getFunctionAddress(ByteBuffer functionName) {
+				long address = getExtensionAddress(memAddress(functionName));
+				if ( address == NULL ) {
+					address = OPENGL.getFunctionAddress(functionName);
+					if ( address == NULL && DEBUG_FUNCTIONS )
+						apiLog("Failed to locate address for GL function " + functionName);
 				}
+
+				return address;
 			}
 
 			@Override

modules/core/src/main/java/org/lwjgl/opengles/GLES.java
@@ -8,6 +8,7 @@ import org.lwjgl.egl.EGL;
 import org.lwjgl.system.*;
 
 import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.*;
 
@@ -105,9 +106,9 @@ public final class GLES {
 
 	private static void create(final SharedLibrary GLES) {
 		try {
-			FunctionProvider functionProvider = new FunctionProvider() {
+			FunctionProvider functionProvider = new FunctionProvider.Default() {
 				@Override
-				public long getFunctionAddress(CharSequence functionName) {
+				public long getFunctionAddress(ByteBuffer functionName) {
 					long address = EGL.getFunctionProvider().getFunctionAddress(functionName);
 					if ( address == NULL ) {
 						address = GLES.getFunctionAddress(functionName);

modules/core/src/main/java/org/lwjgl/system/FunctionProvider.java
@@ -4,20 +4,43 @@
  */
 package org.lwjgl.system;
 
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 /** A provider of native function addresses. */
 public interface FunctionProvider {
 
+	/** {@link CharSequence} version of {@link #getFunctionAddress(ByteBuffer)}. */
+	long getFunctionAddress(CharSequence functionName);
+
 	/**
-	 * Returns the function address of the specified function. If the function
-	 * is not supported by the platform or context, 0L is returned.
+	 * Returns the function address of the specified function. If the function is not supported, returns 0L.
 	 *
-	 * @param functionName the function name to query
+	 * @param functionName the encoded name of the function to query
 	 *
 	 * @return the function address or 0L if the function is not supported
 	 */
-	long getFunctionAddress(CharSequence functionName);
+	long getFunctionAddress(ByteBuffer functionName);
 
 	/** Releases any resource held by this {@link FunctionProvider}. */
 	void free();
+
+	abstract class Default implements FunctionProvider {
+
+		@Override
+		public long getFunctionAddress(CharSequence functionName) {
+			MemoryStack stack = stackPush();
+			try {
+				ByteBuffer encoded = stack.malloc(functionName.length() + 1);
+				memEncodeASCII(functionName, true, encoded);
+				return getFunctionAddress(encoded);
+			} finally {
+				stack.pop();
+			}
+		}
+
+	}
 
 }

commit b1f3bfaa4aa27eaa83c3667c4257e16c13d1f82c

Fix double-free bug when retrieving functions from an OSX library bundle. Close #168
This was caused by 6362edb7d38923200ef4a46f7571420c16f79b38.

modules/core/src/main/java/org/lwjgl/system/macosx/MacOSXLibraryBundle.java
@@ -51,7 +51,7 @@ public class MacOSXLibraryBundle extends MacOSXLibrary {
 	}
 
 	private static long CString2CFString(ByteBuffer name, int encoding) {
-		long string = CFStringCreateWithCStringNoCopy(NULL, name, encoding, kCFAllocatorMalloc());
+		long string = CFStringCreateWithCStringNoCopy(NULL, name, encoding, kCFAllocatorNull());
 		if ( string == NULL )
 			throw new NullPointerException();
 