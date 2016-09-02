7ab68590753b470f2c0af17c20b96f7f6cfce3fb
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-09-02 22:19:21 +0300

    Add new OpenCL extensions
    intel_packed_yuv
    intel_required_subgroup_size

---
+++ modules/templates/src/main/kotlin/org/lwjgl/opencl/templates/intel_packed_yuv.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.opencl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opencl.*
+
+val intel_packed_yuv = "INTELPackedYUV".nativeClassCL("intel_packed_yuv", INTEL) {
+	documentation =
+		"""
+		Native bindings to the $extensionLink extension.
+
+		The purpose of this extension is to provide OpenCL support for packed YUV images.
+
+		Requires ${CL12.link}.
+		"""
+
+	IntConstant(
+		"Accepted as {@code image_channel_order} of {@code cl_image_format}.",
+
+		"YUYV_INTEL"..0x4076,
+		"UYVY_INTEL"..0x4077,
+		"YVYU_INTEL"..0x4078,
+		"VYUY_INTEL"..0x4079
+	)
+}
---
+++ modules/templates/src/main/kotlin/org/lwjgl/opencl/templates/intel_required_subgroup_size.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.opencl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opencl.*
+
+val intel_required_subgroup_size = "INTELRequiredSubgroupSize".nativeClassCL("intel_required_subgroup_size", INTEL) {
+	documentation =
+		"""
+		Native bindings to the $extensionLink extension.
+
+		The goal of this extension is to allow programmers to optionally specify the required subgroup size for a kernel function. This information is
+		important for the correctness of many subgroup algorithms, and in some cases may be used by the compiler to generate more optimal code.
+
+		Requires ${CL21.link} or ${intel_subgroups.link}.
+		"""
+
+	IntConstant(
+		"Accepted as the {@code param_name} parameter of #GetDeviceInfo().",
+
+		"DEVICE_SUB_GROUP_SIZES_INTEL"..0x4108
+	)
+
+	IntConstant(
+		"Accepted as the {@code param_name} parameter of #GetKernelWorkGroupInfo().",
+
+		"KERNEL_SPILL_MEM_SIZE_INTEL"..0x4109
+	)
+
+	IntConstant(
+		"Accepted as the {@code param_name} parameter of #GetKernelSubGroupInfo() and/or #GetKernelSubGroupInfoKHR().",
+
+		"KERNEL_COMPILE_SUB_GROUP_SIZE_INTEL"..0x410A
+	)
+}
