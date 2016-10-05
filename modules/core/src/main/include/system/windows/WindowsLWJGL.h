e4bc12915cb1becac75440b8201d6c239d49b801
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-10-06 00:53:28 +0300

    Bump minimum Windows version to 0x0501 (XP)

--- modules/core/src/main/include/system/windows/WindowsLWJGL.h
+++ modules/core/src/main/include/system/windows/WindowsLWJGL.h
@@ -13,15 +13,15 @@
 #endif
 
 #ifndef _WIN32_WINDOWS
-	#define _WIN32_WINDOWS 0x0500
+	#define _WIN32_WINDOWS 0x0501
 #endif
 
 #ifndef WINVER
-	#define WINVER 0x0500
+	#define WINVER 0x0501
 #endif
 
 #ifndef _WIN32_WINNT
-	#define _WIN32_WINNT 0x0500
+	#define _WIN32_WINNT 0x0501
 #endif
 
 #ifndef UNICODE
