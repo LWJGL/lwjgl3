43e6cf19dc26670e5e6284e7297eb8e04ebadb8b
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-12-30 18:49:40 +0200

    Nullability fixes

--- modules/templates/src/main/kotlin/org/lwjgl/util/tinyfd/templates/tinyfiledialogs.kt
+++ modules/templates/src/main/kotlin/org/lwjgl/util/tinyfd/templates/tinyfiledialogs.kt
@@ -66,12 +66,12 @@ val tinyfiledialogs = "TinyFileDialogs".nativeClass(packageName = TinyFD_PACKAGE
 		"""
 	)
 
-	intb(
+	val messageBox = intb(
 		"messageBox",
 		"Displays a message dialog.",
 
-		const..charUTF8_p.IN("aTitle", "the dialog title"),
-		const..charUTF8_p.IN("aMessage", "the message. It may contain \\n and \\t characters."),
+		nullable..const..charUTF8_p.IN("aTitle", "the dialog title or $NULL"),
+		nullable..const..charUTF8_p.IN("aMessage", "the message or $NULL. It may contain \\n and \\t characters."),
 		const..charASCII_p.IN("aDialogType", "the dialog type", """"ok" "okcancel" "yesno""""),
 		const..charASCII_p.IN("aIconType", "the icon type", """"info" "warning" "error" "question""""),
 		intb.IN("aDefaultButton", "0 for cancel/no, 1 for ok/yes"),
@@ -83,8 +83,8 @@ val tinyfiledialogs = "TinyFileDialogs".nativeClass(packageName = TinyFD_PACKAGE
 		"inputBox",
 		"Displays an input dialog.",
 
-		const..charUTF8_p.IN("aTitle", "the dialog title"),
-		const..charUTF8_p.IN("aMessage", "the message. May NOT contain \\n and \\t characters on Windows."),
+		messageBox["aTitle"],
+		nullable..const..charUTF8_p.IN("aMessage", "the message or $NULL. May NOT contain \\n and \\t characters on Windows."),
 		nullable..const..charUTF8_p.IN("aDefaultInput", "if $NULL it's a password box"),
 
 		returnDoc = "the input value or $NULL on cancel"
@@ -94,8 +94,8 @@ val tinyfiledialogs = "TinyFileDialogs".nativeClass(packageName = TinyFD_PACKAGE
 		"saveFileDialog",
 		"Displays a file save dialog.",
 
-		const..charUTF8_p.IN("aTitle", "the dialog title"),
-		const..charUTF8_p.IN("aDefaultPathAndFile", "the default path and/or file"),
+		messageBox["aTitle"],
+		nullable..const..charUTF8_p.IN("aDefaultPathAndFile", "the default path and/or file or $NULL"),
 		AutoSize("aFilterPatterns")..int.IN("aNumOfFilterPatterns", "the number of patterns in {@code aFilterPatterns}"),
 		nullable..const..charUTF8_p.const.p.IN("aFilterPatterns", """an array of file type patterns ($NULL or {"*.jpg","*.png"}"""),
 		nullable..const..charUTF8_p.IN("aSingleFilterDescription", "$NULL or \"image files\""),
@@ -107,8 +107,8 @@ val tinyfiledialogs = "TinyFileDialogs".nativeClass(packageName = TinyFD_PACKAGE
 		"openFileDialog",
 		"Displays a file open dialog.",
 
-		const..charUTF8_p.IN("aTitle", "the dialog title"),
-		const..charUTF8_p.IN("aDefaultPathAndFile", "the default path and/or file"),
+		messageBox["aTitle"],
+		saveFileDialog["aDefaultPathAndFile"],
 		saveFileDialog["aNumOfFilterPatterns"],
 		saveFileDialog["aFilterPatterns"],
 		saveFileDialog["aSingleFilterDescription"],
@@ -121,15 +121,15 @@ val tinyfiledialogs = "TinyFileDialogs".nativeClass(packageName = TinyFD_PACKAGE
 		"selectFolderDialog",
 		"Displays a folder selection dialog.",
 
-		const..charUTF8_p.IN("aTitle", "the dialog title"),
-		const..charUTF8_p.IN("aDefaultPath", "the default path")
+		messageBox["aTitle"],
+		const..charUTF8_p.IN("aDefaultPath", "the default path or $NULL")
 	)
 
 	const..charUTF8_p(
 		"colorChooser",
 		"Displays a color chooser dialog.",
 
-		const..charUTF8_p.IN("aTitle", "the dialog title"),
+		messageBox["aTitle"],
 		nullable..const..charASCII_p.IN("aDefaultHexRGB", "$NULL or \"\\#FF0000\""),
 		nullable..Check(3)..unsigned_char_p.INOUT("aDefaultRGB", "{ 0 , 255 , 255 }. Used only if {@code aDefaultHexRGB} is $NULL."),
 		Check(3)..unsigned_char_p.OUT("aoResultRGB", "returns the selected color. {@code aDefaultRGB} and {@code aoResultRGB} can be the same array."),
