fe04ad3443769024ff4330f21fdd84374ef3da05
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-02-06 03:48:46 +0200

    Add StackWalkUtil

---
+++ modules/core/src/main/java/org/lwjgl/system/StackWalkUtil.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.system;
+
+import java.util.*;
+
+// Multi-release version: Java 8
+
+/** Stack-walking utilities. */
+final class StackWalkUtil {
+
+	private StackWalkUtil() {
+	}
+
+	static boolean stackWalkIsSameMethod(Object a, Object b) {
+		StackTraceElement f1 = (StackTraceElement)a;
+		StackTraceElement f2 = (StackTraceElement)b;
+
+		return f1.getClassName().equals(f2.getClassName()) && f1.getMethodName() == f2.getMethodName();
+	}
+
+	static StackTraceElement[] stackWalkArray(Object[] a) {
+		return (StackTraceElement[])a;
+	}
+
+	static Object stackWalkGetMethod(int offset, Class<?> after) {
+		StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+
+		for ( int i = offset; i < stacktrace.length; i++ ) {
+			if ( !stacktrace[i].getClassName().startsWith(after.getName()) )
+				return stacktrace[i];
+		}
+
+		throw new IllegalStateException();
+	}
+
+	static Object[] stackWalkGetTrace(int offset, Class<?> after) {
+		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+		int i = Math.min(stackTrace.length, offset + 1);
+		for ( ; i < stackTrace.length; i++ ) {
+			if ( !stackTrace[i].getClassName().startsWith(after.getName()) )
+				break;
+		}
+
+		return Arrays.copyOfRange(stackTrace, i, stackTrace.length);
+	}
+
+}
---
+++ modules/core/src/main/java9/org/lwjgl/system/StackWalkUtil.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.system;
+
+import java.util.*;
+
+// Multi-release version: Java 9
+
+/** Stack-walking utilities. */
+final class StackWalkUtil {
+
+	private StackWalkUtil() {
+	}
+
+	static boolean stackWalkIsSameMethod(Object a, Object b) {
+		StackWalker.StackFrame f1 = (StackWalker.StackFrame)a;
+		StackWalker.StackFrame f2 = (StackWalker.StackFrame)b;
+
+		return f1.getClassName().equals(f2.getClassName()) && f1.getMethodName().equals(f2.getMethodName());
+	}
+
+	static StackTraceElement[] stackWalkArray(Object[] a) {
+		return Arrays.stream(((StackWalker.StackFrame[])a))
+			.map(StackWalker.StackFrame::toStackTraceElement)
+			.toArray(StackTraceElement[]::new);
+	}
+
+	static Object stackWalkGetMethod(int offset, Class<?> after) {
+		return StackWalker.getInstance()
+			.walk(s -> s
+				.skip(offset)
+				.filter(f -> !f.getClassName().startsWith(after.getName()))
+				.findFirst()
+			)
+			.orElseThrow(IllegalStateException::new);
+	}
+
+	static Object[] stackWalkGetTrace(int offset, Class<?> after) {
+		return StackWalker.getInstance()
+			.walk(s -> s
+				.skip(offset)
+				.filter(f -> !f.getClassName().startsWith(after.getName()))
+				.toArray(StackWalker.StackFrame[]::new)
+			);
+	}
+
+}
