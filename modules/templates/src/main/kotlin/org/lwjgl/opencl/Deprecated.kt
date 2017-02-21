b404fbeb4e2c505729d19bca1ba641ce3812e026
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-02-21 18:45:55 +0200

    Add new OpenCL modifier

---
+++ modules/templates/src/main/kotlin/org/lwjgl/opencl/Deprecated.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.opencl
+
+import org.lwjgl.generator.*
+
+/** Marks an OpenCL function as deprecated. */
+class DeprecatedCL(val after: String) : FunctionModifier {
+	override val isSpecial = false
+	override fun validate(func: Func) {
+		if (!func.nativeClass.postfix.isEmpty())
+			throw IllegalArgumentException("The deprecated modifier can only be applied on core functionality.")
+	}
+}
