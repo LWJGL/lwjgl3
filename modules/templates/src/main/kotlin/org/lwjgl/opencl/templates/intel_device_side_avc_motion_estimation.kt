53ee8e5524b7195a7d91f6249d24f556bf8b3fba
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-05-16 22:29:00 +0300

    feat(OpenCL): add new extensions
    - cl_intel_media_block_io
    - cl_intel_planar_yuv
    - cl_intel_device_side_avc_motion_estimation

---
+++ modules/templates/src/main/kotlin/org/lwjgl/opencl/templates/intel_device_side_avc_motion_estimation.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.opencl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opencl.*
+
+val intel_device_side_avc_motion_estimation = "INTELDeviceSideAVCMotionEstimation".nativeClassCL("intel_device_side_avc_motion_estimation", INTEL) {
+    documentation =
+        """
+        Native bindings to the $extensionLink extension.
+
+		Video motion estimation (VME) is defined as of set motion estimation operations that are used to determine the motion vectors, intra estimation angles
+		and macroblock partitioning combination that best describe the transformation to the source macroblock, from blocks in one or more previous reference
+		pictures (inter-prediction), or from other blocks in the same source picture (intra-prediction). It does this by searching for spatial and temporal
+		patterns on the current and various forward and backward reference pictures.
+
+		The goal of this extension is to provide programmers with a fine- grained interface to the AVC VME media sampler in Intel graphics processors. It
+		describes the specification of low-level built-in functions, callable from OpenCL kernels, that facilitate the programming of the VME media sampler to
+		evaluate specific AVC motion estimation operations. If only a coarser-level interface at the level of built-in kernels suffices, then the Intel vendor
+		extensions cl_intel_motion_estimation and cl_advanced_motion_estimation may be considered.
+
+		Built-in functions are defined for all the major operations of the VME media sampler. The major operations of the AVC VME media sampler in Intel
+		Graphics Processors can be described as follows:
+
+		1. Integer motion estimation (IME)
+
+		Perform motion estimation on a given source macroblock in a source image over a single or dual reference window in a reference image, at full-pixel
+		resolution, to determine the best integer motion vectors and their associated distortions, and the best macroblock shape partitioning combination.
+
+		2. Motion estimation refinement (REF)
+
+		Perform refinement operations on the results of IME. The two sub- operations are:
+
+		Fractional motion estimation (FME)
+
+		Perform sub-pixel refinement on the results of an IME operation. half-pixel (HPEL) or quarter-pixel (QPEL) refinements are performed to determine the
+		best sub-pixel motion vectors and their associated distortions.
+
+		Bidirectional motion estimation (BME)
+
+		Perform bidirectional refinement on the results of an IME operation using two reference images to check if the bidirectional mode using two references
+		yields lesser distortions. An FME can optionally be performed implicitly as part of a bidirectional refinement.
+
+		3. Skip and Intra check (SIC)
+
+		Performs the following two sub-operations:
+
+		Skip check (SKC)
+
+		Compute the pixel distortion of a user-specified shape and motion vector combination. The VME media sampler fetches necessary pixels, performs
+		fractional and bidirectional filtering (as necessary), and then computes the distortion between the derived reference and source. The skip decision can
+		optionally be enhanced to include a 4x4 forward transform, the results of which are compared against a user specified threshold to emulate the effects
+		of the forward quantization zeroing effect.
+
+		Intra prediction estimation (IPE)
+
+		Perform intra prediction on a given source macroblock to determine the best intra prediction modes and the best shape partitioning combination.
+
+        Requires ${CL12.link} and ${intel_subgroups.link}.
+        """
+
+    IntConstant(
+        "Accepted as arguments to #GetDeviceInfo().",
+
+        "DEVICE_AVC_ME_VERSION_INTEL"..0x410B,
+        "DEVICE_AVC_ME_SUPPORTS_TEXTURE_SAMPLER_USE_INTEL"..0x410C,
+        "DEVICE_AVC_ME_SUPPORTS_PREEMPTION_INTEL"..0x410D
+    )
+
+    IntConstant(
+        "Additional valid constant values returned by #GetDeviceInfo().",
+
+        "AVC_ME_VERSION_1_INTEL"..0x1
+    )
+}
---
+++ modules/templates/src/main/kotlin/org/lwjgl/opencl/templates/intel_planar_yuv.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.opencl.templates
+
+import org.lwjgl.generator.*
+import org.lwjgl.opencl.*
+
+val intel_planar_yuv = "INTELPlanarYUV".nativeClassCL("intel_planar_yuv", INTEL) {
+    documentation =
+        """
+        Native bindings to the $extensionLink extension.
+
+		The purpose of this extension is to provide OpenCL support for the Planar YUV (YCbCr) image formats. #NV12_INTEL format must be supported; support for
+		other Planar YUV formats that may be defined in this extension is optional.
+
+		The extension introduces two new cl_mem_flags:
+		${ul(
+            """
+            #MEM_NO_ACCESS_INTEL which should be used together with image formats for which device does not support reading from or writing to at OpenCL
+            kernels level, but are still useful in other use cases.
+            """,
+            """
+            #MEM_ACCESS_FLAGS_UNRESTRICTED_INTEL which may be used to relax the memory access rights specified in {@code cl_mem_flags} at memory object
+            creation time and allow to access and modify the contents of the underlying data storage in unrestricted way e.g. by creating another memory object
+            from that memory object or using dedicated device mechanisms.
+            """
+        )}
+
+        Requires ${CL12.link}.
+        """
+
+    IntConstant(
+        "Accepted as {@code cl_mem_flags}.",
+
+        "MEM_NO_ACCESS_INTEL".."1 << 24",
+        "MEM_ACCESS_FLAGS_UNRESTRICTED_INTEL".."1 << 25"
+    )
+
+    IntConstant(
+        "Accepted as {@code image_channel_order} of ##CLImageFormat.",
+
+        "NV12_INTEL"..0x410E
+    )
+
+    IntConstant(
+        "Accepted as arguments passed to #GetDeviceInfo().",
+
+        "DEVICE_PLANAR_YUV_MAX_WIDTH_INTEL"..0x417E,
+        "DEVICE_PLANAR_YUV_MAX_HEIGHT_INTEL"..0x417F
+    )
+}
