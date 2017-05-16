5d8e89fab46b10c66a56c04c9bb4f98cb9d0c250
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-05-16 21:24:01 +0300

    feat(EGL): add new extensions
    - EGL_EXT_compositor
    - EGL_EXT_surface_CTA861_3_metadata
    - EGL_KHR_display_reference
    - EGL_EXT_gl_colorspace_display_p3

---
+++ modules/templates/src/main/kotlin/org/lwjgl/egl/templates/EXT_compositor.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.egl.templates
+
+import org.lwjgl.egl.*
+import org.lwjgl.generator.*
+
+val EXT_compositor = "EXTCompositor".nativeClassEGL("EXT_compositor", postfix = EXT) {
+    documentation =
+        """
+        Native bindings to the $registryLink extension.
+
+		This extension allows for the composition of multiple windows within a multi-partition EGL system. The extension allows a primary EGLContext and window
+		to be created for each display. All other windows are created using non-displayable surfaces. A handle to each off-screen window is provided to the
+		primary EGLContext to allow the composition of all non-displayable windows on a single display.
+
+		For each display, there is one EGLContext which has access to create on screen windows, this is call the primary context for this display. All other
+		EGLContexts are referred to as secondary contexts.
+
+		This extension requires a multi-partition EGL driver to support asynchronous rendering of off screen surfaces.
+
+		Information assurance is provided by preventing context and surface creation by unregistered contexts and by preventing the non-primary contexts and
+		surfaces from rendering to the display.
+        """
+
+    IntConstant(
+		"New attributes accepted by the {@code attrib_list} argument of #CreateContext().",
+
+		"PRIMARY_COMPOSITOR_CONTEXT_EXT"..0x3460
+	)
+
+	IntConstant(
+		"New attributes accepted by the {@code attrib_list} argument of #CreateContext() and #CreateWindowSurface().",
+
+		"EXTERNAL_REF_ID_EXT"..0x3461
+	)
+
+	IntConstant(
+		"New attributes accepted by the {@code policy} argument of #CompositorSwapPolicyEXT().",
+
+		"COMPOSITOR_DROP_NEWEST_FRAME_EXT"..0x3462,
+		"COMPOSITOR_KEEP_NEWEST_FRAME_EXT"..0x3463
+	)
+
+    EGLBoolean(
+        "CompositorSetContextListEXT",
+        "",
+
+        const..EGLint_p.IN("external_ref_ids", ""),
+        AutoSize("external_ref_ids")..EGLint.IN("num_entries", "")
+    )
+
+    EGLBoolean(
+        "CompositorSetContextAttributesEXT",
+        "",
+
+        EGLint.IN("external_ref_id", ""),
+        const..EGLint_p.IN("context_attributes", ""),
+        AutoSize("context_attributes")..EGLint.IN("num_entries", "")
+    )
+
+    EGLBoolean(
+        "CompositorSetWindowListEXT",
+        "",
+
+        EGLint.IN("external_ref_id", ""),
+        const..EGLint_p.IN("external_win_ids", ""),
+        AutoSize("external_win_ids")..EGLint.IN("num_entries", "")
+    )
+
+    EGLBoolean(
+        "CompositorSetWindowAttributesEXT",
+        "",
+
+        EGLint.IN("external_win_id", ""),
+        const..EGLint_p.IN("window_attributes", ""),
+        AutoSize("window_attributes")..EGLint.IN("num_entries", "")
+    )
+
+    EGLBoolean(
+        "CompositorBindTexWindowEXT",
+        "",
+
+        EGLint.IN("external_win_id", "")
+    )
+
+    EGLBoolean(
+        "CompositorSetSizeEXT",
+        "",
+
+        EGLint.IN("external_win_id", ""),
+        EGLint.IN("width", ""),
+        EGLint.IN("height", "")
+    )
+
+    EGLBoolean(
+        "CompositorSwapPolicyEXT",
+        "",
+
+        EGLint.IN("external_win_id", ""),
+        EGLint.IN("policy", "")
+    )
+}
---
+++ modules/templates/src/main/kotlin/org/lwjgl/egl/templates/EXT_gl_colorspace_display_p3.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.egl.templates
+
+import org.lwjgl.egl.*
+import org.lwjgl.generator.*
+
+val EXT_gl_colorspace_display_p3_linear = "EXTGLColorspaceDisplayP3Linear".nativeClassEGL("EXT_gl_colorspace_display_p3_linear", postfix = EXT) {
+    documentation =
+        """
+        Native bindings to the $registryLink extension.
+
+        Applications that want to use the Display-P3 color space (DCI-P3 primaries and linear or sRGB-like transfer function) can use this extension to
+		communicate to the platform that framebuffer contents represent colors in the Display-P3 color space. The application is responsible for producing
+		appropriate framebuffer contents, but will typically use built-in sRGB encoding in OpenGL and OpenGL ES to accomplish this.
+
+        Requires ${KHR_gl_colorspace.link}.
+        """
+
+    IntConstant(
+        "Accepted as attribute values for #GL_COLORSPACE by #CreateWindowSurface(), #CreatePbufferSurface() and #CreatePixmapSurface().",
+
+        "GL_COLORSPACE_DISPLAY_P3_LINEAR_EXT"..0x3362
+    )
+}
+
+val EXT_gl_colorspace_display_p3 = "EXTGLColorspaceDisplayP3".nativeClassEGL("EXT_gl_colorspace_display_p3", postfix = EXT) {
+    documentation =
+        """
+        See ${EXT_gl_colorspace_display_p3_linear.link}.
+        """
+
+    IntConstant(
+        "Accepted as attribute values for #GL_COLORSPACE by #CreateWindowSurface(), #CreatePbufferSurface() and #CreatePixmapSurface().",
+
+        "GL_COLORSPACE_DISPLAY_P3_EXT"..0x3363
+    )
+}
