d1f3352c926996c712cbc7ad3b89e9decdfaa1f5
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-11-08 13:56:45 +0200

    style(demo): bgfx demo code cleanup

--- modules/core/src/test/java/org/lwjgl/demo/bgfx/HelloBGFX.java
+++ modules/core/src/test/java/org/lwjgl/demo/bgfx/HelloBGFX.java
@@ -42,8 +42,7 @@ public final class HelloBGFX {
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
         long window = glfwCreateWindow(width, height, "25-C99", 0, 0);
-
-        if (window == 0) {
+        if (window == NULL) {
             throw new RuntimeException("Error creating GLFW window");
         }
 
@@ -56,18 +55,12 @@ public final class HelloBGFX {
                     platformData.nwh(GLFWNativeX11.glfwGetX11Window(window));
                     break;
                 case MACOSX:
-                    platformData.ndt(NULL);
                     platformData.nwh(GLFWNativeCocoa.glfwGetCocoaWindow(window));
                     break;
                 case WINDOWS:
-                    platformData.ndt(NULL);
                     platformData.nwh(GLFWNativeWin32.glfwGetWin32Window(window));
                     break;
             }
-
-            platformData.context(NULL);
-            platformData.backBuffer(NULL);
-            platformData.backBufferDS(NULL);
 
             bgfx_set_platform_data(platformData);
         }
@@ -75,9 +68,9 @@ public final class HelloBGFX {
         if (!bgfx_init(renderer, pciId, 0, null, null)) {
             throw new RuntimeException("Error initializing bgfx renderer");
         }
-
         bgfx_reset(width, height, reset);
 
+        // Enable debug text.
         bgfx_set_debug(debug);
 
         bgfx_set_view_clear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
@@ -85,32 +78,29 @@ public final class HelloBGFX {
         ByteBuffer logo = Logo.createLogo();
 
         while (!glfwWindowShouldClose(window)) {
-
             glfwPollEvents();
 
+            // Set view 0 default viewport.
             bgfx_set_view_rect(0, 0, 0, width, height);
 
+            // This dummy draw call is here to make sure that view 0 is cleared
+            // if no other draw calls are submitted to view 0.
             bgfx_touch(0);
 
+            // Use debug font to print information about this example.
             bgfx_dbg_text_clear(0, false);
-
             bgfx_dbg_text_image(Math.max(width / 2 / 8, 20) - 20,
                 Math.max(height / 2 / 16, 6) - 6,
                 40,
                 12,
                 logo,
-                160);
+                160
+            );
+            bgfx_dbg_text_printf(0, 1, 0x4f, "bgfx/examples/25-c99");
+            bgfx_dbg_text_printf(0, 2, 0x6f, "Description: Initialization and debug text with C99 API.");
 
-            bgfx_dbg_text_printf(0,
-                1,
-                0x4f,
-                "bgfx/examples/25-c99");
-
-            bgfx_dbg_text_printf(0,
-                2,
-                0x6f,
-                "Description: Initialization and debug text with C99 API.");
-
+            // Advance to next frame. Rendering thread will be kicked to
+            // process submitted rendering primitives.
             bgfx_frame(false);
         }
 
