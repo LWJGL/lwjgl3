be216b1933e65972854e907adb3a0bef8f3342a0
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-01-28 02:51:36 +0200

    fix(bgfx): update demo to new VGA color order

--- modules/samples/src/test/java/org/lwjgl/demo/bgfx/HelloBGFX.java
+++ modules/samples/src/test/java/org/lwjgl/demo/bgfx/HelloBGFX.java
@@ -108,8 +108,13 @@ public final class HelloBGFX {
                 logo,
                 160
             );
-            bgfx_dbg_text_printf(0, 1, 0x4f, "bgfx/examples/25-c99");
-            bgfx_dbg_text_printf(0, 2, 0x6f, "Description: Initialization and debug text with C99 API.");
+            bgfx_dbg_text_printf(0, 1, 0x1f, "bgfx/examples/25-c99");
+            bgfx_dbg_text_printf(0, 2, 0x3f, "Description: Initialization and debug text with C99 API.");
+
+            bgfx_dbg_text_printf(0, 3, 0x0f, "Color can be changed with ANSI \u001b[9;me\u001b[10;ms\u001b[11;mc\u001b[12;ma\u001b[13;mp\u001b[14;me\u001b[0m code too.");
+
+            bgfx_dbg_text_printf(80, 4, 0x0f, "\u001b[;0m    \u001b[;1m    \u001b[; 2m    \u001b[; 3m    \u001b[; 4m    \u001b[; 5m    \u001b[; 6m    \u001b[; 7m    \u001b[0m");
+            bgfx_dbg_text_printf(80, 5, 0x0f, "\u001b[;8m    \u001b[;9m    \u001b[;10m    \u001b[;11m    \u001b[;12m    \u001b[;13m    \u001b[;14m    \u001b[;15m    \u001b[0m");
 
             // Advance to next frame. Rendering thread will be kicked to
             // process submitted rendering primitives.
