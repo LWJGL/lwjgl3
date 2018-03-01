660a05f567ee275cac975405d5e611ae59c0e188
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-03-01 14:09:26 +0200

    build(linux): update disabled GCC warnings

--- modules/lwjgl/core/src/main/c/linux/LinuxConfig.h
+++ modules/lwjgl/core/src/main/c/linux/LinuxConfig.h
@@ -9,6 +9,8 @@
 #define DISABLE_WARNINGS() \
     _Pragma("GCC diagnostic push")
     _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+    _Pragma("GCC diagnostic ignored \"-Wunused-but-set-variable\"")
+    _Pragma("GCC diagnostic ignored \"-Wsign-compare\"")
 
 #define ENABLE_WARNINGS() \
     _Pragma("GCC diagnostic pop")
