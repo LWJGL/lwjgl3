c79674ff65d34b3d44731d236d45c1d18e4031db
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-04-11 18:56:41 +0300

    feat(glfw): add native API for the Wayland backend

---
+++ modules/lwjgl/core/src/templates/kotlin/core/linux/WaylandTypes.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package core.linux
+
+import org.lwjgl.generator.*
+
+val wl_display = "struct wl_display".opaque
+val wl_output = "struct wl_output".opaque
+val wl_surface = "struct wl_surface".opaque
---
+++ modules/lwjgl/glfw/src/main/kotlin/glfw/templates/GLFWNativeWayland.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package glfw.templates
+
+import org.lwjgl.generator.*
+import glfw.*
+import core.linux.*
+
+val GLFWNativeWayland = "GLFWNativeWayland".nativeClass(Module.GLFW, nativeSubPath = "linux", prefix = "GLFW", binding = GLFW_BINDING_DELEGATE) {
+    documentation = "Native bindings to the GLFW library's Wayland native access functions."
+
+    wl_display.p(
+        "GetWaylandDisplay",
+        """
+        Returns the {@code struct wl_display*} used by GLFW.
+
+        This function may be called from any thread. Access is not synchronized.
+        """,
+        returnDoc = "the {@code struct wl_display*} used by GLFW, or #NULL if an error occurred.",
+        since = "version 3.2"
+    )
+
+    wl_output.p(
+        "GetWaylandMonitor",
+        """
+        Returns the {@code struct wl_output*} of the specified monitor.
+
+        This function may be called from any thread. Access is not synchronized.
+        """,
+
+        GLFWmonitor.p.IN("monitor", ""),
+        returnDoc = "the {@code struct wl_output*} of the specified monitor, or #NULL if an error occurred.",
+        since = "version 3.2"
+    )
+
+    wl_surface.p(
+        "GetWaylandWindow",
+        """
+        Returns the main {@code struct wl_surface*} of the specified window.
+
+        This function may be called from any thread. Access is not synchronized.
+        """,
+
+        GLFWwindow.p.IN("window", ""),
+        returnDoc = "the main {@code struct wl_surface*} of the specified window, or #NULL if an error occurred.",
+        since = "version 3.2"
+    )
+}
