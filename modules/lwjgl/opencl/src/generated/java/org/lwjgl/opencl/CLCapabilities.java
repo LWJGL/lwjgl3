0b772c38cc3f6e84786eb7b229cdc8afca6bc72f
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-08-31 15:24:44 +0300

    feat(opencl): update cl_arm_core_id description

--- modules/lwjgl/opencl/src/generated/java/org/lwjgl/opencl/CLCapabilities.java
+++ modules/lwjgl/opencl/src/generated/java/org/lwjgl/opencl/CLCapabilities.java
@@ -590,12 +590,15 @@ public class CLCapabilities {
     /**
      * When true, the <a target="_blank" href="http://www.khronos.org/registry/OpenCL/extensions/arm/cl_arm_get_core_id.txt">arm_core_id</a> extension is supported.
      * 
-     * <p>This extension provides a built-in function ({@code uint arm_get_core_id( void )}) which returns the physical core id (OpenCL Compute Unit) that a
-     * work-group is running on. This value is uniform for a work-group.</p>
+     * <p>This extension provides a built-in function ({@code uint arm_get_core_id( void )}) which returns a unique ID for the compute unit that a work-group is
+     * running on. This value is uniform for a work-group.</p>
      * 
      * <p>This value can be used for a core-specific cache or atomic pool where the storage is required to be in global memory and persistent (but not ordered)
      * between work-groups. This does not provide any additional ordering on top of the existing guarantees between workgroups, nor does it provide any
      * guarantee of concurrent execution.</p>
+     * 
+     * <p>The IDs for the compute units may not be consecutive and applications must make sure they allocate enough memory to accommodate all the compute units
+     * present on the device. A device info query allows the application to know the IDs associated with the compute units on a given device.</p>
      */
     public final boolean cl_arm_core_id;
     /** When true, {@link ARMPrintf} is supported. */
--- modules/lwjgl/opencl/src/templates/kotlin/opencl/templates/ExtensionFlags.kt
+++ modules/lwjgl/opencl/src/templates/kotlin/opencl/templates/ExtensionFlags.kt
@@ -527,12 +527,15 @@ val arm_core_id = EXT_FLAG.nativeClassCL("arm_core_id", ARM) {
         """
         When true, the ${extensionLink("arm_get_core_id")} extension is supported.
 
-        This extension provides a built-in function ({@code uint arm_get_core_id( void )}) which returns the physical core id (OpenCL Compute Unit) that a
-        work-group is running on. This value is uniform for a work-group.
+        This extension provides a built-in function ({@code uint arm_get_core_id( void )}) which returns a unique ID for the compute unit that a work-group is
+        running on. This value is uniform for a work-group.
 
         This value can be used for a core-specific cache or atomic pool where the storage is required to be in global memory and persistent (but not ordered)
         between work-groups. This does not provide any additional ordering on top of the existing guarantees between workgroups, nor does it provide any
         guarantee of concurrent execution.
+
+        The IDs for the compute units may not be consecutive and applications must make sure they allocate enough memory to accommodate all the compute units
+        present on the device. A device info query allows the application to know the IDs associated with the compute units on a given device.
         """
 }
 
