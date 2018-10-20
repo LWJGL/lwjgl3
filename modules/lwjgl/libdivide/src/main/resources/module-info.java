3a62cbd081f60600edc2c9d4f7dd3a54572a99b3
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-10-20 20:58:41 +0300

    fix(libdivide): module name and Linux build

--- modules/lwjgl/libdivide/src/main/resources/module-info.java
+++ modules/lwjgl/libdivide/src/main/resources/module-info.java
@@ -2,7 +2,7 @@
  * Copyright LWJGL. All rights reserved.
  * License terms: https://www.lwjgl.org/license
  */
-module org.lwjgl.lmdb {
+module org.lwjgl.libdivide {
     requires transitive org.lwjgl;
 
     exports org.lwjgl.util.libdivide;
