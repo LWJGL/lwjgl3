a799f60af89649acb1cc8ca73d1b096c9c35a95f
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-10-31 11:42:17 +0200

    feat(jawt): add embedded frame demo

---
+++ modules/samples/src/test/java/org/lwjgl/demo/system/jawt/EmbeddedFrameDemo.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.demo.system.jawt;
+
+import org.lwjgl.demo.opengl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import static org.lwjgl.demo.system.jawt.EmbeddedFrameUtil.*;
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/** AWT frame embedded in GLFW using jawt. */
+public final class EmbeddedFrameDemo {
+
+    private static boolean focused;
+
+    private EmbeddedFrameDemo() {
+    }
+
+    public static void main(String[] args) {
+        if (Platform.get() == Platform.MACOSX) {
+            Toolkit.getDefaultToolkit();
+        }
+
+        GLFWErrorCallback.createPrint().set();
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize glfw");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+        if (Platform.get() == Platform.MACOSX) {
+            glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
+        }
+
+        int WIDTH  = 300;
+        int HEIGHT = 300;
+
+        long window = glfwCreateWindow(WIDTH, HEIGHT, "GLFW Gears Demo", NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        int FRAME_WIDTH  = 200;
+        int FRAME_HEIGHT = 32;
+
+        Frame ef = embeddedFrameCreate(window);
+        System.out.println("Embedded frame implementation: " + ef.getClass());
+        //ef.setFocusableWindowState(false);
+
+        JPanel pane = new JPanel();
+        pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));
+        ef.add(pane);
+
+        JButton btn = new JButton("JButton");
+        btn.addActionListener(e -> System.out.println("Button clicked!"));
+        pane.add(btn);
+
+        JTextField text = new JTextField("JTextField", 20);
+        pane.add(text);
+
+        glfwSetFramebufferSizeCallback(window, (window1, width, height) -> {
+            if (width == 0 || height == 0) {
+                return;
+            }
+
+            float f = height / (float)width;
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glFrustum(-1.0f, 1.0f, -f, f, 5.0f, 100.0f);
+            glMatrixMode(GL_MODELVIEW);
+
+            glViewport(0, 0, width, height);
+
+            EventQueue.invokeLater(() -> {
+                embeddedFrameSetBounds(ef, width - FRAME_WIDTH, height - FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT);
+                ef.invalidate();
+            });
+        });
+        glfwSetWindowFocusCallback(window, (windowHnd, focused) -> {
+            EmbeddedFrameDemo.focused = focused;
+            System.out.println("GLFW " + (focused ? "gained" : "lost") + " focus");
+        });
+        glfwSetMouseButtonCallback(window, (windowHnd, button, action, mods) -> {
+            if (!focused) {
+                glfwFocusWindow(windowHnd);
+            }
+        });
+        glfwSetKeyCallback(window, (windowHnd, key, scancode, action, mods) -> {
+            if (action != GLFW_RELEASE) {
+                return;
+            }
+
+            switch (key) {
+                case GLFW_KEY_ESCAPE:
+                    glfwSetWindowShouldClose(windowHnd, true);
+                    break;
+                default:
+                    System.out.println("GLFW key: " + key);
+            }
+        });
+
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+        Callback debugProc = GLUtil.setupDebugMessageCallback();
+
+        AbstractGears gears = new AbstractGears();
+        gears.initGLState();
+
+        glfwSetWindowRefreshCallback(window, windowHnd -> {
+            gears.renderLoop();
+            glfwSwapBuffers(windowHnd);
+        });
+
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+
+            gears.renderLoop();
+
+            glfwSwapBuffers(window);
+        }
+
+        // clean-up
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        EventQueue.invokeLater(() -> {
+            ef.dispose();
+            latch.countDown();
+        });
+
+        try {
+            // Wait for the frame to be disposed
+            while (!latch.await(10, TimeUnit.MILLISECONDS)) {
+                glfwPollEvents();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (debugProc != null) {
+            debugProc.free();
+        }
+
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        Objects.requireNonNull(glfwSetErrorCallback(null)).free();
+    }
+
+}
---
+++ modules/samples/src/test/java/org/lwjgl/demo/system/jawt/EmbeddedFrameUtil.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.demo.system.jawt;
+
+import org.lwjgl.system.*;
+import org.lwjgl.system.jawt.JAWT;
+import org.lwjgl.system.macosx.*;
+
+import java.awt.*;
+import java.lang.reflect.*;
+import java.util.regex.*;
+
+import static org.lwjgl.glfw.GLFWNativeCocoa.*;
+import static org.lwjgl.glfw.GLFWNativeWin32.*;
+import static org.lwjgl.glfw.GLFWNativeX11.*;
+import static org.lwjgl.system.JNI.*;
+import static org.lwjgl.system.jawt.JAWTFunctions.*;
+import static org.lwjgl.system.macosx.ObjCRuntime.*;
+
+final class EmbeddedFrameUtil {
+
+    private static final int JAVA_VERSION;
+
+    private static final JAWT awt;
+
+    static {
+        Pattern p = Pattern.compile("^(?:1[.])?([1-9][0-9]*)[.-]");
+        Matcher m = p.matcher(System.getProperty("java.version"));
+
+        if (!m.find()) {
+            throw new IllegalStateException("Failed to parse java.version");
+        }
+
+        JAVA_VERSION = Integer.parseInt(m.group(1));
+
+        awt = JAWT.calloc();
+        awt.version(JAVA_VERSION < 9 ? JAWT_VERSION_1_4 : JAWT_VERSION_9);
+        if (!JAWT_GetAWT(awt)) {
+            throw new RuntimeException("GetAWT failed");
+        }
+    }
+
+    private EmbeddedFrameUtil() {
+    }
+
+    private static String getEmbeddedFrameImpl() {
+        switch (Platform.get()) {
+            case LINUX:
+                return "sun.awt.X11.XEmbeddedFrame";
+            case MACOSX:
+                return "sun.lwawt.macosx.CViewEmbeddedFrame";
+            case WINDOWS:
+                return "sun.awt.windows.WEmbeddedFrame";
+            default:
+                throw new IllegalStateException();
+        }
+    }
+
+    private static long getEmbeddedFrameHandle(long window) {
+        switch (Platform.get()) {
+            case LINUX:
+                return glfwGetX11Window(window);
+            case MACOSX:
+                long objc_msgSend = ObjCRuntime.getLibrary().getFunctionAddress("objc_msgSend");
+                return invokePPP(objc_msgSend, glfwGetCocoaWindow(window), sel_getUid("contentView"));
+            case WINDOWS:
+                return glfwGetWin32Window(window);
+            default:
+                throw new IllegalStateException();
+        }
+    }
+
+    static Frame embeddedFrameCreate(long window) {
+        if (JAVA_VERSION < 9) {
+            try {
+                @SuppressWarnings("unchecked")
+                Class<? extends Frame> EmdeddedFrame = (Class<? extends Frame>)Class.forName(getEmbeddedFrameImpl());
+                Constructor<? extends Frame> c = EmdeddedFrame.getConstructor(long.class);
+
+                return c.newInstance(getEmbeddedFrameHandle(window));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return nJAWT_CreateEmbeddedFrame(awt.CreateEmbeddedFrame(), getEmbeddedFrameHandle(window));
+        }
+    }
+
+    static void embeddedFrameSynthesizeWindowActivation(Frame embeddedFrame, boolean doActivate) {
+        if (JAVA_VERSION < 9) {
+            try {
+                embeddedFrame
+                    .getClass()
+                    .getMethod("synthesizeWindowActivation", boolean.class)
+                    .invoke(embeddedFrame, doActivate);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            JAWT_SynthesizeWindowActivation(awt.SynthesizeWindowActivation(), embeddedFrame, doActivate);
+        }
+    }
+
+    static void embeddedFrameSetBounds(Frame embeddedFrame, int x, int y, int width, int height) {
+        if (JAVA_VERSION < 9) {
+            try {
+                Method setLocationPrivate = embeddedFrame
+                    .getClass()
+                    .getSuperclass()
+                    .getDeclaredMethod("setBoundsPrivate", int.class, int.class, int.class, int.class);
+                setLocationPrivate.setAccessible(true);
+                setLocationPrivate.invoke(embeddedFrame, x, y, width, height);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            JAWT_SetBounds(awt.SetBounds(), embeddedFrame, x, y, width, height);
+        }
+    }
+
+}
