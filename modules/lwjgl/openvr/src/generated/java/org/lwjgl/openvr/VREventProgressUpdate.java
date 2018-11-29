15975b7ad773028d96f646c807a5a9ccda6a0d4c
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-11-29 22:54:29 +0200

    fix(openvr): add new generated sources

---
+++ modules/lwjgl/openvr/src/generated/java/org/lwjgl/openvr/VREventProgressUpdate.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ * MACHINE GENERATED FILE, DO NOT EDIT
+ */
+package org.lwjgl.openvr;
+
+import javax.annotation.*;
+
+import java.nio.*;
+
+import org.lwjgl.system.*;
+
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * <h3>Layout</h3>
+ * 
+ * <pre><code>
+ * struct VREvent_ProgressUpdate_t {
+ *     uint64_t ulApplicationPropertyContainer;
+ *     uint64_t pathDevice;
+ *     uint64_t pathInputSource;
+ *     uint64_t pathProgressAction;
+ *     uint64_t pathIcon;
+ *     float fProgress;
+ * }</code></pre>
+ */
+@NativeType("struct VREvent_ProgressUpdate_t")
+public class VREventProgressUpdate extends Struct {
+
+    /** The struct size in bytes. */
+    public static final int SIZEOF;
+
+    /** The struct alignment in bytes. */
+    public static final int ALIGNOF;
+
+    /** The struct member offsets. */
+    public static final int
+        ULAPPLICATIONPROPERTYCONTAINER,
+        PATHDEVICE,
+        PATHINPUTSOURCE,
+        PATHPROGRESSACTION,
+        PATHICON,
+        FPROGRESS;
+
+    static {
+        Layout layout = __struct(
+            __member(8),
+            __member(8),
+            __member(8),
+            __member(8),
+            __member(8),
+            __member(4)
+        );
+
+        SIZEOF = layout.getSize();
+        ALIGNOF = layout.getAlignment();
+
+        ULAPPLICATIONPROPERTYCONTAINER = layout.offsetof(0);
+        PATHDEVICE = layout.offsetof(1);
+        PATHINPUTSOURCE = layout.offsetof(2);
+        PATHPROGRESSACTION = layout.offsetof(3);
+        PATHICON = layout.offsetof(4);
+        FPROGRESS = layout.offsetof(5);
+    }
+
+    /**
+     * Creates a {@link VREventProgressUpdate} instance at the current position of the specified {@link ByteBuffer} container. Changes to the buffer's content will be
+     * visible to the struct instance and vice versa.
+     *
+     * <p>The created instance holds a strong reference to the container object.</p>
+     */
+    public VREventProgressUpdate(ByteBuffer container) {
+        super(memAddress(container), __checkContainer(container, SIZEOF));
+    }
+
+    @Override
+    public int sizeof() { return SIZEOF; }
+
+    /** Returns the value of the {@code ulApplicationPropertyContainer} field. */
+    @NativeType("uint64_t")
+    public long ulApplicationPropertyContainer() { return nulApplicationPropertyContainer(address()); }
+    /** Returns the value of the {@code pathDevice} field. */
+    @NativeType("uint64_t")
+    public long pathDevice() { return npathDevice(address()); }
+    /** Returns the value of the {@code pathInputSource} field. */
+    @NativeType("uint64_t")
+    public long pathInputSource() { return npathInputSource(address()); }
+    /** Returns the value of the {@code pathProgressAction} field. */
+    @NativeType("uint64_t")
+    public long pathProgressAction() { return npathProgressAction(address()); }
+    /** Returns the value of the {@code pathIcon} field. */
+    @NativeType("uint64_t")
+    public long pathIcon() { return npathIcon(address()); }
+    /** Returns the value of the {@code fProgress} field. */
+    public float fProgress() { return nfProgress(address()); }
+
+    // -----------------------------------
+
+    /** Returns a new {@link VREventProgressUpdate} instance for the specified memory address. */
+    public static VREventProgressUpdate create(long address) {
+        return wrap(VREventProgressUpdate.class, address);
+    }
+
+    /** Like {@link #create(long) create}, but returns {@code null} if {@code address} is {@code NULL}. */
+    @Nullable
+    public static VREventProgressUpdate createSafe(long address) {
+        return address == NULL ? null : wrap(VREventProgressUpdate.class, address);
+    }
+
+    /**
+     * Create a {@link VREventProgressUpdate.Buffer} instance at the specified memory.
+     *
+     * @param address  the memory address
+     * @param capacity the buffer capacity
+     */
+    public static VREventProgressUpdate.Buffer create(long address, int capacity) {
+        return wrap(Buffer.class, address, capacity);
+    }
+
+    /** Like {@link #create(long, int) create}, but returns {@code null} if {@code address} is {@code NULL}. */
+    @Nullable
+    public static VREventProgressUpdate.Buffer createSafe(long address, int capacity) {
+        return address == NULL ? null : wrap(Buffer.class, address, capacity);
+    }
+
+    // -----------------------------------
+
+    /** Unsafe version of {@link #ulApplicationPropertyContainer}. */
+    public static long nulApplicationPropertyContainer(long struct) { return UNSAFE.getLong(null, struct + VREventProgressUpdate.ULAPPLICATIONPROPERTYCONTAINER); }
+    /** Unsafe version of {@link #pathDevice}. */
+    public static long npathDevice(long struct) { return UNSAFE.getLong(null, struct + VREventProgressUpdate.PATHDEVICE); }
+    /** Unsafe version of {@link #pathInputSource}. */
+    public static long npathInputSource(long struct) { return UNSAFE.getLong(null, struct + VREventProgressUpdate.PATHINPUTSOURCE); }
+    /** Unsafe version of {@link #pathProgressAction}. */
+    public static long npathProgressAction(long struct) { return UNSAFE.getLong(null, struct + VREventProgressUpdate.PATHPROGRESSACTION); }
+    /** Unsafe version of {@link #pathIcon}. */
+    public static long npathIcon(long struct) { return UNSAFE.getLong(null, struct + VREventProgressUpdate.PATHICON); }
+    /** Unsafe version of {@link #fProgress}. */
+    public static float nfProgress(long struct) { return UNSAFE.getFloat(null, struct + VREventProgressUpdate.FPROGRESS); }
+
+    // -----------------------------------
+
+    /** An array of {@link VREventProgressUpdate} structs. */
+    public static class Buffer extends StructBuffer<VREventProgressUpdate, Buffer> {
+
+        private static final VREventProgressUpdate ELEMENT_FACTORY = VREventProgressUpdate.create(-1L);
+
+        /**
+         * Creates a new {@link VREventProgressUpdate.Buffer} instance backed by the specified container.
+         *
+         * Changes to the container's content will be visible to the struct buffer instance and vice versa. The two buffers' position, limit, and mark values
+         * will be independent. The new buffer's position will be zero, its capacity and its limit will be the number of bytes remaining in this buffer divided
+         * by {@link VREventProgressUpdate#SIZEOF}, and its mark will be undefined.
+         *
+         * <p>The created buffer instance holds a strong reference to the container object.</p>
+         */
+        public Buffer(ByteBuffer container) {
+            super(container, container.remaining() / SIZEOF);
+        }
+
+        public Buffer(long address, int cap) {
+            super(address, null, -1, 0, cap, cap);
+        }
+
+        Buffer(long address, @Nullable ByteBuffer container, int mark, int pos, int lim, int cap) {
+            super(address, container, mark, pos, lim, cap);
+        }
+
+        @Override
+        protected Buffer self() {
+            return this;
+        }
+
+        @Override
+        protected VREventProgressUpdate getElementFactory() {
+            return ELEMENT_FACTORY;
+        }
+
+        /** Returns the value of the {@code ulApplicationPropertyContainer} field. */
+        @NativeType("uint64_t")
+        public long ulApplicationPropertyContainer() { return VREventProgressUpdate.nulApplicationPropertyContainer(address()); }
+        /** Returns the value of the {@code pathDevice} field. */
+        @NativeType("uint64_t")
+        public long pathDevice() { return VREventProgressUpdate.npathDevice(address()); }
+        /** Returns the value of the {@code pathInputSource} field. */
+        @NativeType("uint64_t")
+        public long pathInputSource() { return VREventProgressUpdate.npathInputSource(address()); }
+        /** Returns the value of the {@code pathProgressAction} field. */
+        @NativeType("uint64_t")
+        public long pathProgressAction() { return VREventProgressUpdate.npathProgressAction(address()); }
+        /** Returns the value of the {@code pathIcon} field. */
+        @NativeType("uint64_t")
+        public long pathIcon() { return VREventProgressUpdate.npathIcon(address()); }
+        /** Returns the value of the {@code fProgress} field. */
+        public float fProgress() { return VREventProgressUpdate.nfProgress(address()); }
+
+    }
+
+}
---
+++ modules/lwjgl/openvr/src/generated/java/org/lwjgl/openvr/VRSkeletalSummaryData.java
@@ -0,0 +1,301 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ * MACHINE GENERATED FILE, DO NOT EDIT
+ */
+package org.lwjgl.openvr;
+
+import javax.annotation.*;
+
+import java.nio.*;
+
+import org.lwjgl.*;
+import org.lwjgl.system.*;
+
+import static org.lwjgl.system.Checks.*;
+import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.system.MemoryStack.*;
+
+/**
+ * Contains summary information about the current skeletal pose.
+ * 
+ * <h3>Member documentation</h3>
+ * 
+ * <ul>
+ * <li>{@code flFingerCurl} &ndash; 
+ * The amount that each finger is 'curled' inwards towards the palm.
+ * 
+ * <p>In the case of the thumb, this represents how much the thumb is wrapped around the fist. 0 means straight, 1 means fully curled.</p></li>
+ * <li>{@code flFingerSplay} &ndash; 
+ * The amount that each pair of adjacent fingers are separated.
+ * 
+ * <p>0 means the digits are touching, 1 means they are fully separated.</p></li>
+ * </ul>
+ * 
+ * <h3>Layout</h3>
+ * 
+ * <pre><code>
+ * struct VRSkeletalSummaryData_t {
+ *     float flFingerCurl[5];
+ *     float flFingerSplay[4];
+ * }</code></pre>
+ */
+@NativeType("struct VRSkeletalSummaryData_t")
+public class VRSkeletalSummaryData extends Struct implements NativeResource {
+
+    /** The struct size in bytes. */
+    public static final int SIZEOF;
+
+    /** The struct alignment in bytes. */
+    public static final int ALIGNOF;
+
+    /** The struct member offsets. */
+    public static final int
+        FLFINGERCURL,
+        FLFINGERSPLAY;
+
+    static {
+        Layout layout = __struct(
+            __array(4, 5),
+            __array(4, 4)
+        );
+
+        SIZEOF = layout.getSize();
+        ALIGNOF = layout.getAlignment();
+
+        FLFINGERCURL = layout.offsetof(0);
+        FLFINGERSPLAY = layout.offsetof(1);
+    }
+
+    /**
+     * Creates a {@link VRSkeletalSummaryData} instance at the current position of the specified {@link ByteBuffer} container. Changes to the buffer's content will be
+     * visible to the struct instance and vice versa.
+     *
+     * <p>The created instance holds a strong reference to the container object.</p>
+     */
+    public VRSkeletalSummaryData(ByteBuffer container) {
+        super(memAddress(container), __checkContainer(container, SIZEOF));
+    }
+
+    @Override
+    public int sizeof() { return SIZEOF; }
+
+    /** Returns a {@link FloatBuffer} view of the {@code flFingerCurl} field. */
+    @NativeType("float[5]")
+    public FloatBuffer flFingerCurl() { return nflFingerCurl(address()); }
+    /** Returns the value at the specified index of the {@code flFingerCurl} field. */
+    public float flFingerCurl(int index) { return nflFingerCurl(address(), index); }
+    /** Returns a {@link FloatBuffer} view of the {@code flFingerSplay} field. */
+    @NativeType("float[4]")
+    public FloatBuffer flFingerSplay() { return nflFingerSplay(address()); }
+    /** Returns the value at the specified index of the {@code flFingerSplay} field. */
+    public float flFingerSplay(int index) { return nflFingerSplay(address(), index); }
+
+    // -----------------------------------
+
+    /** Returns a new {@link VRSkeletalSummaryData} instance allocated with {@link MemoryUtil#memAlloc memAlloc}. The instance must be explicitly freed. */
+    public static VRSkeletalSummaryData malloc() {
+        return wrap(VRSkeletalSummaryData.class, nmemAllocChecked(SIZEOF));
+    }
+
+    /** Returns a new {@link VRSkeletalSummaryData} instance allocated with {@link MemoryUtil#memCalloc memCalloc}. The instance must be explicitly freed. */
+    public static VRSkeletalSummaryData calloc() {
+        return wrap(VRSkeletalSummaryData.class, nmemCallocChecked(1, SIZEOF));
+    }
+
+    /** Returns a new {@link VRSkeletalSummaryData} instance allocated with {@link BufferUtils}. */
+    public static VRSkeletalSummaryData create() {
+        ByteBuffer container = BufferUtils.createByteBuffer(SIZEOF);
+        return wrap(VRSkeletalSummaryData.class, memAddress(container), container);
+    }
+
+    /** Returns a new {@link VRSkeletalSummaryData} instance for the specified memory address. */
+    public static VRSkeletalSummaryData create(long address) {
+        return wrap(VRSkeletalSummaryData.class, address);
+    }
+
+    /** Like {@link #create(long) create}, but returns {@code null} if {@code address} is {@code NULL}. */
+    @Nullable
+    public static VRSkeletalSummaryData createSafe(long address) {
+        return address == NULL ? null : wrap(VRSkeletalSummaryData.class, address);
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated with {@link MemoryUtil#memAlloc memAlloc}. The instance must be explicitly freed.
+     *
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer malloc(int capacity) {
+        return wrap(Buffer.class, nmemAllocChecked(__checkMalloc(capacity, SIZEOF)), capacity);
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated with {@link MemoryUtil#memCalloc memCalloc}. The instance must be explicitly freed.
+     *
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer calloc(int capacity) {
+        return wrap(Buffer.class, nmemCallocChecked(capacity, SIZEOF), capacity);
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated with {@link BufferUtils}.
+     *
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer create(int capacity) {
+        ByteBuffer container = __create(capacity, SIZEOF);
+        return wrap(Buffer.class, memAddress(container), capacity, container);
+    }
+
+    /**
+     * Create a {@link VRSkeletalSummaryData.Buffer} instance at the specified memory.
+     *
+     * @param address  the memory address
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer create(long address, int capacity) {
+        return wrap(Buffer.class, address, capacity);
+    }
+
+    /** Like {@link #create(long, int) create}, but returns {@code null} if {@code address} is {@code NULL}. */
+    @Nullable
+    public static VRSkeletalSummaryData.Buffer createSafe(long address, int capacity) {
+        return address == NULL ? null : wrap(Buffer.class, address, capacity);
+    }
+
+    // -----------------------------------
+
+    /** Returns a new {@link VRSkeletalSummaryData} instance allocated on the thread-local {@link MemoryStack}. */
+    public static VRSkeletalSummaryData mallocStack() {
+        return mallocStack(stackGet());
+    }
+
+    /** Returns a new {@link VRSkeletalSummaryData} instance allocated on the thread-local {@link MemoryStack} and initializes all its bits to zero. */
+    public static VRSkeletalSummaryData callocStack() {
+        return callocStack(stackGet());
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData} instance allocated on the specified {@link MemoryStack}.
+     *
+     * @param stack the stack from which to allocate
+     */
+    public static VRSkeletalSummaryData mallocStack(MemoryStack stack) {
+        return wrap(VRSkeletalSummaryData.class, stack.nmalloc(ALIGNOF, SIZEOF));
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData} instance allocated on the specified {@link MemoryStack} and initializes all its bits to zero.
+     *
+     * @param stack the stack from which to allocate
+     */
+    public static VRSkeletalSummaryData callocStack(MemoryStack stack) {
+        return wrap(VRSkeletalSummaryData.class, stack.ncalloc(ALIGNOF, 1, SIZEOF));
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated on the thread-local {@link MemoryStack}.
+     *
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer mallocStack(int capacity) {
+        return mallocStack(capacity, stackGet());
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated on the thread-local {@link MemoryStack} and initializes all its bits to zero.
+     *
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer callocStack(int capacity) {
+        return callocStack(capacity, stackGet());
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated on the specified {@link MemoryStack}.
+     *
+     * @param stack the stack from which to allocate
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer mallocStack(int capacity, MemoryStack stack) {
+        return wrap(Buffer.class, stack.nmalloc(ALIGNOF, capacity * SIZEOF), capacity);
+    }
+
+    /**
+     * Returns a new {@link VRSkeletalSummaryData.Buffer} instance allocated on the specified {@link MemoryStack} and initializes all its bits to zero.
+     *
+     * @param stack the stack from which to allocate
+     * @param capacity the buffer capacity
+     */
+    public static VRSkeletalSummaryData.Buffer callocStack(int capacity, MemoryStack stack) {
+        return wrap(Buffer.class, stack.ncalloc(ALIGNOF, capacity, SIZEOF), capacity);
+    }
+
+    // -----------------------------------
+
+    /** Unsafe version of {@link #flFingerCurl}. */
+    public static FloatBuffer nflFingerCurl(long struct) { return memFloatBuffer(struct + VRSkeletalSummaryData.FLFINGERCURL, 5); }
+    /** Unsafe version of {@link #flFingerCurl(int) flFingerCurl}. */
+    public static float nflFingerCurl(long struct, int index) {
+        return UNSAFE.getFloat(null, struct + VRSkeletalSummaryData.FLFINGERCURL + check(index, 5) * 4);
+    }
+    /** Unsafe version of {@link #flFingerSplay}. */
+    public static FloatBuffer nflFingerSplay(long struct) { return memFloatBuffer(struct + VRSkeletalSummaryData.FLFINGERSPLAY, 4); }
+    /** Unsafe version of {@link #flFingerSplay(int) flFingerSplay}. */
+    public static float nflFingerSplay(long struct, int index) {
+        return UNSAFE.getFloat(null, struct + VRSkeletalSummaryData.FLFINGERSPLAY + check(index, 4) * 4);
+    }
+
+    // -----------------------------------
+
+    /** An array of {@link VRSkeletalSummaryData} structs. */
+    public static class Buffer extends StructBuffer<VRSkeletalSummaryData, Buffer> implements NativeResource {
+
+        private static final VRSkeletalSummaryData ELEMENT_FACTORY = VRSkeletalSummaryData.create(-1L);
+
+        /**
+         * Creates a new {@link VRSkeletalSummaryData.Buffer} instance backed by the specified container.
+         *
+         * Changes to the container's content will be visible to the struct buffer instance and vice versa. The two buffers' position, limit, and mark values
+         * will be independent. The new buffer's position will be zero, its capacity and its limit will be the number of bytes remaining in this buffer divided
+         * by {@link VRSkeletalSummaryData#SIZEOF}, and its mark will be undefined.
+         *
+         * <p>The created buffer instance holds a strong reference to the container object.</p>
+         */
+        public Buffer(ByteBuffer container) {
+            super(container, container.remaining() / SIZEOF);
+        }
+
+        public Buffer(long address, int cap) {
+            super(address, null, -1, 0, cap, cap);
+        }
+
+        Buffer(long address, @Nullable ByteBuffer container, int mark, int pos, int lim, int cap) {
+            super(address, container, mark, pos, lim, cap);
+        }
+
+        @Override
+        protected Buffer self() {
+            return this;
+        }
+
+        @Override
+        protected VRSkeletalSummaryData getElementFactory() {
+            return ELEMENT_FACTORY;
+        }
+
+        /** Returns a {@link FloatBuffer} view of the {@code flFingerCurl} field. */
+        @NativeType("float[5]")
+        public FloatBuffer flFingerCurl() { return VRSkeletalSummaryData.nflFingerCurl(address()); }
+        /** Returns the value at the specified index of the {@code flFingerCurl} field. */
+        public float flFingerCurl(int index) { return VRSkeletalSummaryData.nflFingerCurl(address(), index); }
+        /** Returns a {@link FloatBuffer} view of the {@code flFingerSplay} field. */
+        @NativeType("float[4]")
+        public FloatBuffer flFingerSplay() { return VRSkeletalSummaryData.nflFingerSplay(address()); }
+        /** Returns the value at the specified index of the {@code flFingerSplay} field. */
+        public float flFingerSplay(int index) { return VRSkeletalSummaryData.nflFingerSplay(address(), index); }
+
+    }
+
+}
