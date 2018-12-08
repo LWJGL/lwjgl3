015c9a95b5ac98fa53e7e5ba359541b12e6798de
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-12-08 22:23:59 +0200

    build: bump version to 3.2.2

--- modules/lwjgl/core/src/main/java/org/lwjgl/Version.java
+++ modules/lwjgl/core/src/main/java/org/lwjgl/Version.java
@@ -13,7 +13,7 @@ public final class Version {
     public static final int
         VERSION_MAJOR    = 3,
         VERSION_MINOR    = 2,
-        VERSION_REVISION = 1;
+        VERSION_REVISION = 2;
 
     /** The development state of the current build. */
     public static final BuildType BUILD_TYPE = BuildType.STABLE;
