109d2d96675d69446902914dfd40fccf26deaa9a
Ioannis Tsakpinis <iotsakp@gmail.com>
2019-04-09 12:49:26 +0300

    fix(xxhash): documentation

--- modules/lwjgl/xxhash/src/templates/kotlin/xxhash/templates/xxhash.kt
+++ modules/lwjgl/xxhash/src/templates/kotlin/xxhash/templates/xxhash.kt
@@ -25,72 +25,20 @@ ENABLE_WARNINGS()""")
 
         <h3>XXH3</h3>
 
- * XXH3 is a new hash algorithm,
- * featuring vastly improved speed performance
- * for both small and large inputs.
- * A full speed analysis will be published,
- * it requires a lot more space than this comment can handle.
- * In general, expect XXH3 to run about ~2x faster on large inputs,
- * and >3x faster on small ones, though exact difference depend on platform.
- *
- * The algorithm is portable, will generate the same hash on all platforms.
- * It benefits greatly from vectorization units, but does not require it.
- *
- * XXH3 offers 2 variants, _64bits and _128bits.
- * When only 64 bits are needed, prefer calling the _64bits variant :
- * it reduces the amount of mixing, resulting in faster speed on small inputs.
- * It's also generally simpler to manipulate a scalar type than a struct.
- * Note : the low 64-bit field of the _128bits variant is the same as _64bits result.
- *
- * The XXH3 algorithm is still considered experimental.
- * It's possible to use it for ephemeral data, but avoid storing long-term values for later re-use.
- * While labelled experimental, the produced result can still change between versions.
- *
- * The API currently supports one-shot hashing only.
- * The full version will include streaming capability, and canonical representation
- * Long term optional feature may include custom secret keys, and secret key generation.
- *
- * There are still a number of opened questions that community can influence during the experimental period.
- * I'm trying to list a few of them below, though don't consider this list as complete.
- *
- * - 128-bits output type : currently defined as a structure of 2 64-bits fields.
- *                          That's because 128-bit values do not exist in C standard.
- *                          Note that it means that, at byte level, result is not identical depending on endianess.
- *                          However, at field level, they are identical on all platforms.
- *                          The canonical representation will solve the issue of identical byte-level representation across platforms,
- *                          which is necessary for serialization.
- *                          Would there be a better representation for a 128-bit hash result ?
- *                          Are the names of the inner 64-bit fields important ? Should they be changed ?
- *
- * - Canonical representation : for the 64-bit variant, canonical representation is the same as XXH64() (aka big-endian).
- *                          What should it be for the 128-bit variant ?
- *                          Since it's no longer a scalar value, big-endian representation is no longer an obvious choice.
- *                          One possibility : represent it as the concatenation of two 64-bits canonical representation (aka 2x big-endian)
- *                          Another one : represent it in the same order as natural order in the struct for little-endian platforms.
- *                                        Less consistent with existing convention for XXH32/XXH64, but may be more natural for little-endian platforms.
- *
- * - Associated functions for 128-bit hash : simple things, such as checking if 2 hashes are equal, become more difficult with struct.
- *                          Granted, it's not terribly difficult to create a comparator, but it's still a workload.
- *                          Would it be beneficial to declare and define a comparator function for XXH128_hash_t ?
- *                          Are there other operations on XXH128_hash_t which would be desirable ?
- *
- * - Variant compatibility : The low 64-bit field of the _128bits variant is the same as the result of _64bits.
- *                          This is not a compulsory behavior. It just felt that it "wouldn't hurt", and might even help in some (unidentified) cases.
- *                          But it might influence the design of XXH128_hash_t, in ways which may block other possibilities.
- *                          Good idea, bad idea ?
- *
- * - Seed type for 128-bits variant : currently, it's a single 64-bit value, like the 64-bit variant.
- *                          It could be argued that it's more logical to offer a 128-bit seed input parameter for a 128-bit hash.
- *                          Although it's also more difficult to use, since it requires to declare and pass a structure instead of a value.
- *                          It would either replace current choice, or add a new one.
- *                          Farmhash, for example, offers both variants (the 128-bits seed variant is called `doubleSeed`).
- *                          If both 64-bit and 128-bit seeds are possible, which variant should be called XXH128 ?
- *
- * - Result for len==0 : Currently, the result of hashing a zero-length input is the seed.
- *                          This mimics the behavior of a crc : in which case, a seed is effectively an accumulator, so it's not updated if input is empty.
- *                          Consequently, by default, when no seed specified, it returns zero. That part seems okay (it used to be a request for XXH32/XXH64).
- *                          But is it still fine to return the seed when the seed is non-zero ?
- *                          Are there use case which would depend on this behavior, or would prefer a mixing of the seed ?
+        XXH3 is a new hash algorithm, featuring vastly improved speed performance for both small and large inputs.
+
+        In general, expect XXH3 to run about ~2x faster on large inputs, and &gt;3x faster on small ones, though exact difference depend on platform. The
+        algorithm is portable, will generate the same hash on all platforms. It benefits greatly from vectorization units, but does not require it.
+
+        XXH3 offers 2 variants, {@code _64bits} and {@code _128bits}. When only 64 bits are needed, prefer calling the {@code _64bits} variant: it reduces the
+        amount of mixing, resulting in faster speed on small inputs. It's also generally simpler to manipulate a scalar type than a struct. Note: the low
+        64-bit field of the {@code _128bits} variant is the same as {@code _64bits} result.
+
+        The XXH3 algorithm is still considered experimental. It's possible to use it for ephemeral data, but avoid storing long-term values for later re-use.
+        While labelled experimental, the produced result can still change between versions.
+
+        The API currently supports one-shot hashing only. The full version will include streaming capability, and canonical representation. Long term optional
+        feature may include custom secret keys, and secret key generation.
         """
 
     EnumConstant(
