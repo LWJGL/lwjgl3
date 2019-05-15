647abc41e4555d8c11d0179344a125484ee49f1a
Ioannis Tsakpinis <iotsakp@gmail.com>
2019-05-15 18:20:21 +0300

    build(shaderc): add module-info

---
+++ modules/lwjgl/shaderc/src/main/resources/module-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+module org.lwjgl.shaderc {
+    requires transitive org.lwjgl;
+
+    exports org.lwjgl.util.shaderc;
+}
