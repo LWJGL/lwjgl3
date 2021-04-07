86e13fd1b8310863c0dbae05415a8a85377425ce
Alex Foster <alexfoster@hotmail.ca>
2021-04-07 19:36:41 -0400

    vpx linux

--- modules/lwjgl/core/src/generated/java/org/lwjgl/system/JNI.java
+++ modules/lwjgl/core/src/generated/java/org/lwjgl/system/JNI.java
@@ -79,7 +79,6 @@ public final class JNI {
     public static native int invokePI(long param0, float param1, long __functionAddress);
     public static native int invokePI(long param0, int param1, long __functionAddress);
     public static native int invokePI(long param0, short param1, long __functionAddress);
-    public static native int invokePI(long param0, boolean param1, long __functionAddress);
     public static native int invokePI(short param0, long param1, long __functionAddress);
     public static native int invokePI(long param0, int param1, float param2, long __functionAddress);
     public static native int invokePI(long param0, int param1, int param2, long __functionAddress);
