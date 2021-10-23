116f78a46955390606702929ce3ffab6304cec3d
Ioannis Tsakpinis <iotsakp@gmail.com>
2021-10-23 17:09:12 +0300

    build(tinyexr): fix miniz compilation

--- modules/lwjgl/yoga/src/main/c/Yoga.cpp
+++ modules/lwjgl/yoga/src/main/c/Yoga.cpp
@@ -4324,7 +4324,7 @@ YOGA_EXPORT void YGConfigSetExperimentalFeatureEnabled(
   config->experimentalFeatures[feature] = enabled;
 }
 
-inline bool YGConfigIsExperimentalFeatureEnabled(
+bool YGConfigIsExperimentalFeatureEnabled(
     const YGConfigRef config,
     const YGExperimentalFeature feature) {
   return config->experimentalFeatures[feature];
