8d9e1d41693290baf6e3e64e29088469e770cdb2
Marc <marcflament@free.fr>
2022-02-20 18:42:15 +0100

    fix(OpenCL) KERNEL_ARG_TYPE_QUALIFIER constant

--- modules/lwjgl/opencl/src/generated/java/org/lwjgl/opencl/CL12.java
+++ modules/lwjgl/opencl/src/generated/java/org/lwjgl/opencl/CL12.java
@@ -165,7 +165,7 @@ public class CL12 extends CL11 {
         CL_KERNEL_ARG_ADDRESS_QUALIFIER = 0x1196,
         CL_KERNEL_ARG_ACCESS_QUALIFIER  = 0x1197,
         CL_KERNEL_ARG_TYPE_NAME         = 0x1198,
-        CL_KERNEL_ARG_TYPE_QUALIFIER    = 0x1999,
+        CL_KERNEL_ARG_TYPE_QUALIFIER    = 0x1199,
         CL_KERNEL_ARG_NAME              = 0x119A;
 
     /** cl_kernel_arg_address_qualifier */
