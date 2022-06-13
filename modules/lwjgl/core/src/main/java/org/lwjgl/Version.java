4e654a0783f0c6f13452e548d6e1efb77cc099a7
Christian Stein <sormuras@gmail.com>
2022-06-13 12:29:05 +0200

    fix(core) impl version lookup on module path
    - Package::getImplementationVersion is unspecified when loading a JAR
      file from the module path. The new multi-release code uses the module
      descriptor to retrieve the implementation version.
    - Restored code that retrieves the implementation version from the JAR
      manifest, as a fallback. Also added the suggested fix from #644, when
      running on a GraalVM native image.
    - Changed the LWJGL version string to match the Java version format.
    
    Close #770
    
    Co-authored-by: Leon Linhart <themrmilchmann@gmail.com>

--- modules/lwjgl/core/src/main/java/org/lwjgl/Version.java
+++ modules/lwjgl/core/src/main/java/org/lwjgl/Version.java
@@ -4,7 +4,11 @@
  */
 package org.lwjgl;
 
+import javax.annotation.*;
+import java.io.*;
+import java.net.*;
 import java.util.*;
+import java.util.jar.*;
 
 /** This class can be used to query the LWJGL version. */
 public final class Version {
@@ -18,18 +22,19 @@ public final class Version {
     /** The development state of the current build. */
     public static final BuildType BUILD_TYPE = BuildType.STABLE;
 
-    private static final String version =
+    private static final String versionPlain =
         String.valueOf(VERSION_MAJOR) +
         '.' + VERSION_MINOR +
-        '.' + VERSION_REVISION + BUILD_TYPE.postfix +
-        ' ' + Optional.ofNullable(Version.class.getPackage().getImplementationVersion()).orElse("SNAPSHOT");
+        '.' + VERSION_REVISION + BUILD_TYPE.postfix;
+
+    private static final String version = versionPlain + VersionImpl.find();
 
     private Version() {
     }
 
     public static void main(String[] args) {
         System.out.println(version);
-        System.err.println(version.substring(0, version.indexOf(' ')));
+        System.err.println(versionPlain);
     }
 
     /** Returns the LWJGL version. */
@@ -50,6 +55,72 @@ public final class Version {
 
         BuildType(String postfix) {
             this.postfix = postfix;
+        }
+    }
+
+    static String createImplementation(String specVersion, String implVersion) {
+        String build = "+" + (implVersion.startsWith("build ") && 6 < implVersion.length() ? implVersion.substring(6) : implVersion);
+
+        if (specVersion.contains("SNAPSHOT") || specVersion.contains("snapshot")) {
+            return "-snapshot" + build;
+        }
+
+        return build;
+    }
+
+    @Nullable
+    static String findImplementationFromManifest() {
+        ClassLoader classLoader = Version.class.getClassLoader();
+
+        URL url = classLoader.getResource("org/lwjgl/Version.class");
+        if (url != null) {
+            String classURL = url.toString();
+            try {
+                if (classURL.startsWith("jar:")) { // running on standard JDK
+                    URL manifest = Version.class.getResource("/" + JarFile.MANIFEST_NAME);
+
+                    String version = readImplementationFromManifest(Objects.requireNonNull(manifest));
+                    if (version != null) {
+                        return version;
+                    }
+                } else if (classURL.startsWith("resource:")) { // running on GraalVM native image
+                    Enumeration<URL> e = classLoader.getResources(JarFile.MANIFEST_NAME);
+                    while (e.hasMoreElements()) {
+                        String version = readImplementationFromManifest(e.nextElement());
+                        if (version != null) {
+                            return version;
+                        }
+                    }
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
+        return null;
+    }
+
+    @Nullable
+    private static String readImplementationFromManifest(URL url) {
+        try (InputStream stream = url.openStream()) {
+            Attributes attribs = new Manifest(stream).getMainAttributes();
+
+            // make sure this is the manifest from lwjgl.jar
+            if (!"lwjgl".equals(attribs.getValue(Attributes.Name.IMPLEMENTATION_TITLE))) {
+                return null;
+            }
+            if (!"lwjgl.org".equals(attribs.getValue(Attributes.Name.IMPLEMENTATION_VENDOR))) {
+                return null;
+            }
+
+            String specVersion = attribs.getValue(Attributes.Name.SPECIFICATION_VERSION);
+            String implVersion = attribs.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+            if (specVersion == null || implVersion == null) {
+                return null;
+            }
+
+            return createImplementation(specVersion, implVersion);
+        } catch (Exception ignored) {
+            return null;
         }
     }
 
