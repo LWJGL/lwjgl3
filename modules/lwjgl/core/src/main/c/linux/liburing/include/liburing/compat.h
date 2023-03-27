773cddeaf584aa9bff8b4358e7c0e6baff3c45c5
Ioannis Tsakpinis <iotsakp@gmail.com>
2023-03-27 12:46:56 +0300

    fix(core) liburing build

--- modules/lwjgl/core/src/main/c/linux/liburing/include/liburing/compat.h
+++ modules/lwjgl/core/src/main/c/linux/liburing/include/liburing/compat.h
@@ -7,6 +7,9 @@
 #include <stdint.h>
 #include <sys/stat.h>
 
+// LWJGL: skip <linux/time_types.h> include in io_uring.h
+#define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H
+
 typedef int __kernel_rwf_t;
 
 struct __kernel_timespec {
