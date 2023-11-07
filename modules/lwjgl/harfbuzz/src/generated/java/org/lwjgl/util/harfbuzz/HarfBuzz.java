b4a3e7d9471ce4ae94c0c205a405cb0269ffac99
Ioannis Tsakpinis <iotsakp@gmail.com>
2023-11-07 20:52:31 +0200

    feat(harfbuzz) update to 8.2.2

--- modules/lwjgl/harfbuzz/src/generated/java/org/lwjgl/util/harfbuzz/HarfBuzz.java
+++ modules/lwjgl/harfbuzz/src/generated/java/org/lwjgl/util/harfbuzz/HarfBuzz.java
@@ -1488,9 +1488,9 @@ public class HarfBuzz {
 
     public static final int HB_VERSION_MINOR = 2;
 
-    public static final int HB_VERSION_MICRO = 0;
+    public static final int HB_VERSION_MICRO = 2;
 
-    public static final String HB_VERSION_STRING = "8.2.0";
+    public static final String HB_VERSION_STRING = "8.2.2";
 
     protected HarfBuzz() {
         throw new UnsupportedOperationException();
