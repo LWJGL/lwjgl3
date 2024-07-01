e575cd93f14de60cdbce4149f1486363d6181057
Daniel <55595431+boredhuman@users.noreply.github.com>
2024-07-01 18:42:42 -0400

    fix(OpenGL) Added missing constant GL_BLEND_COLOR and GL_BLEND_EQUATION to GL14

--- modules/lwjgl/opengl/src/generated/java/org/lwjgl/opengl/GL14.java
+++ modules/lwjgl/opengl/src/generated/java/org/lwjgl/opengl/GL14.java
@@ -53,11 +53,17 @@ public class GL14 extends GL13 {
         GL_CONSTANT_ALPHA           = 0x8003,
         GL_ONE_MINUS_CONSTANT_ALPHA = 0x8004;
 
+    /** Accepted by {@link GL11#glGetFloat*} to get the constant color used in blending, which can be set using {@link GL14#glBlendColor}. */
+    public static final int GL_BLEND_COLOR = 0x8005;
+
     /** Accepted by the {@code mode} parameter of BlendEquation. */
     public static final int
         GL_FUNC_ADD = 0x8006,
         GL_MIN      = 0x8007,
         GL_MAX      = 0x8008;
+
+    /** Accepted by {@link GL11#glGetInteger*} to get the current blend equation, which can be set using {@link GL14#glBlendEquation}. */
+    public static final int GL_BLEND_EQUATION = 0x8009;
 
     /** Accepted by the {@code mode} parameter of BlendEquation. */
     public static final int
