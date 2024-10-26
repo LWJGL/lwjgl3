924d3ea2b3e96b77c37370fa5a32b65dfe355a0b
Ioannis Tsakpinis <iotsakp@gmail.com>
2024-10-27 00:34:38 +0300

    perf(core) restore custom loop in Java 17 memcpy

--- modules/lwjgl/core17/src/main/java/org/lwjgl/system/MultiReleaseMemCopy.java
+++ modules/lwjgl/core17/src/main/java/org/lwjgl/system/MultiReleaseMemCopy.java
@@ -6,6 +6,7 @@ package org.lwjgl.system;
 
 import static org.lwjgl.system.APIUtil.*;
 import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.system.Pointer.*;
 
 final class MultiReleaseMemCopy {
 
@@ -17,6 +18,14 @@ final class MultiReleaseMemCopy {
     }
 
     static void copy(long src, long dst, long bytes) {
+        if (BITS64 && bytes <= 160) {
+            // A custom Java loop is fastest at small sizes, approximately up to 160 bytes.
+            if (((src | dst) & 7) == 0) {
+                // both src and dst are aligned to 8 bytes
+                memCopyAligned64(src, dst, (int)bytes & 0xFF);
+                return;
+            }
+        }
         // Much better performance on Java 16+
         UNSAFE.copyMemory(null, src, null, dst, bytes);
     }
