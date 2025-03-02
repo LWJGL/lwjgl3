12a30612fc96d5708eeb923362b83c0120c3d1f1
Ioannis Tsakpinis <iotsakp@gmail.com>
2025-03-02 14:21:50 +0200

    feat(fmod) update to 2.03.06

--- modules/lwjgl/fmod/src/templates/kotlin/fmod/templates/FMOD.kt
+++ modules/lwjgl/fmod/src/templates/kotlin/fmod/templates/FMOD.kt
@@ -14,8 +14,8 @@ val FMOD = "FMOD".nativeClass(
     binding = simpleBinding(Module.FMOD, libraryName = "FMOD", libraryExpression = "Configuration.FMOD_LIBRARY_NAME, \"fmod\"")
 ) {
 
-    IntConstant("VERSION"..0x00020304)
-    IntConstant("BUILDNUMBER"..147563)
+    IntConstant("VERSION"..0x00020306)
+    IntConstant("BUILDNUMBER"..149358)
 
     IntConstant(
         "DEBUG_LEVEL_NONE"..0x00000000,
