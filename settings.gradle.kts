af40e8ce8ef1a1fc6c5b54ff97a9954b2cc250e6
Niklas Johansson <dwarfley.private@gmail.com>
2025-08-08 19:56:43 +0200

    build: change root project name to lwjgl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@
  * Copyright LWJGL. All rights reserved.
  * License terms: https://www.lwjgl.org/license
  */
-rootProject.name = "Root"
+rootProject.name = "lwjgl"
 
 includeBuild("publish/build-logic")
 
