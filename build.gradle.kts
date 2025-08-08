020c720f454d66c3f07360dc9eb13063b9c0d046
Niklas Johansson <dwarfley.private@gmail.com>
2025-08-08 21:04:10 +0200

    extract platform logic to convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,74 +8,42 @@ plugins {
     `maven-publish`
     signing
 }
+
 /*
-publishing {
-    publications {
-        /*
-        Ideally, we'd have the following structure:
-        -------------------------------------------
-        lwjgl
-            lwjgl-windows (depends on lwjgl)
-        glfw (depends on lwjgl)
-            glfw-windows (depends on glfw & lwjgl-windows)
-        stb (depends on lwjgl)
-            stb-windows (depends on stb & lwjgl-windows)
-        -------------------------------------------
-        If a user wanted to use GLFW + stb in their project, running on
-        the Windows platform, they'd only have to define glfw-windows
-        and stb-windows as dependencies. This would automatically
-        resolve stb, glfw, lwjgl and lwjgl-windows as transitive
-        dependencies. Unfortunately, it is not possible to define such
-        a relationship between Maven artifacts when using classifiers.
-        A method to make this work is make the natives-<arch> classified
-        JARs separate artifacts. We do not do it for aesthetic reasons.
-        Instead, we assume that a tool is available (on the LWJGL website)
-        that automatically generates POM/Gradle dependency structures for
-        projects wanting to use LWJGL. The output is going to be verbose;
-        the above example is going to look like this in Gradle:
-        -------------------------------------------
-        compile 'org.lwjgl:lwjgl:$lwjglVersion' // NOTE: this is optional, all binding artifacts have a dependency on lwjgl
-            compile 'org.lwjgl:lwjgl:$lwjglVersion:natives-$lwjglArch'
-        compile 'org.lwjgl:lwjgl-glfw:$lwjglVersion'
-            compile 'org.lwjgl:lwjgl-glfw:$lwjglVersion:natives-$lwjglArch'
-        compile 'org.lwjgl:lwjgl-stb:$lwjglVersion'
-            compile 'org.lwjgl:lwjgl-stb:$lwjglVersion:natives-$lwjglArch'
-        -------------------------------------------
-        and a whole lot more verbose in Maven. Hopefully, the automation
-        is going to alleviate the pain.
-         */
+Ideally, we'd have the following structure:
+-------------------------------------------
+lwjgl
+    lwjgl-windows (depends on lwjgl)
+glfw (depends on lwjgl)
+    glfw-windows (depends on glfw & lwjgl-windows)
+stb (depends on lwjgl)
+    stb-windows (depends on stb & lwjgl-windows)
+-------------------------------------------
+If a user wanted to use GLFW + stb in their project, running on
+the Windows platform, they'd only have to define glfw-windows
+and stb-windows as dependencies. This would automatically
+resolve stb, glfw, lwjgl and lwjgl-windows as transitive
+dependencies. Unfortunately, it is not possible to define such
+a relationship between Maven artifacts when using classifiers.
+A method to make this work is make the natives-<arch> classified
+JARs separate artifacts. We do not do it for aesthetic reasons.
+Instead, we assume that a tool is available (on the LWJGL website)
+that automatically generates POM/Gradle dependency structures for
+projects wanting to use LWJGL. The output is going to be verbose;
+the above example is going to look like this in Gradle:
+-------------------------------------------
+compile 'org.lwjgl:lwjgl:$lwjglVersion' // NOTE: this is optional, all binding artifacts have a dependency on lwjgl
+    compile 'org.lwjgl:lwjgl:$lwjglVersion:natives-$lwjglArch'
+compile 'org.lwjgl:lwjgl-glfw:$lwjglVersion'
+    compile 'org.lwjgl:lwjgl-glfw:$lwjglVersion:natives-$lwjglArch'
+compile 'org.lwjgl:lwjgl-stb:$lwjglVersion'
+    compile 'org.lwjgl:lwjgl-stb:$lwjglVersion:natives-$lwjglArch'
+-------------------------------------------
+and a whole lot more verbose in Maven. Hopefully, the automation
+is going to alleviate the pain.
+ */
 
-        create<MavenPublication>("lwjglBOM") {
-            from(components["javaPlatform"])
-            artifactId = "lwjgl-bom"
-
-            pom {
-                packaging = "pom"
-
-                withXml {
-                    asElement().getElementsByTagName("dependencyManagement").item(0).apply {
-                        asElement().getElementsByTagName("dependencies").item(0).apply {
-                            Artifacts.values().forEach { module ->
-                                module.platforms.forEach {
-                                    ownerDocument.createElement("dependency").also(::appendChild).apply {
-                                        appendChild(ownerDocument.createElement("groupId").also(::appendChild).apply { textContent = "org.lwjgl" })
-                                        appendChild(ownerDocument.createElement("artifactId").also(::appendChild).apply { textContent = module.artifact })
-                                        appendChild(ownerDocument.createElement("version").also(::appendChild).apply { textContent = project.version as String })
-                                        appendChild(ownerDocument.createElement("classifier").also(::appendChild).apply { textContent = "natives-${it.classifier}" })
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    // Workaround for https://github.com/gradle/gradle/issues/7529
-                    asNode()
-                }
-            }
-        }
-    }
-}
-
+/*
 val copyArchives = tasks.create<Copy>("copyArchives") {
     from("bin/RELEASE")
     include("**")
@@ -85,11 +53,4 @@ val copyArchives = tasks.create<Copy>("copyArchives") {
 tasks.withType<Sign> {
     dependsOn(copyArchives)
 }
-
-dependencies {
-    constraints {
-        Artifacts.values().forEach { module ->
-            api("org.lwjgl:${module.artifact}:$version")
-        }
-    }
-}*/
+*/
--- publish/build-logic/lwjgl-convention/src/main/kotlin/lwjgl-platform.gradle.kts
+++ publish/build-logic/lwjgl-convention/src/main/kotlin/lwjgl-platform.gradle.kts
@@ -6,3 +6,25 @@ plugins {
     `java-platform`
     id("lwjgl-publishing")
 }
+
+lwjglPublication.all {
+    from(components["javaPlatform"])
+
+    pom {
+        packaging = "pom"
+    }
+}
+
+val lwjglModules = mutableListOf<String>()
+
+project(":lwjgl-modules").subprojects.forEach { subProject ->
+    lwjglModules.add(subProject.name)
+}
+
+dependencies {
+    constraints {
+        lwjglModules.forEach { module ->
+            api(project(":lwjgl-modules:${module}"))
+        }
+    }
+}
