d9105fc93b7ae67d07f77aefdb0bfc0b0951a5ea
Ioannis Tsakpinis <iotsakp@gmail.com>
2025-08-20 20:58:51 +0300

    chore(Vulkan) add pNext test
    Demonstrates how pNext prepending works.

--- modules/lwjgl/vulkan/src/test/java/org/lwjgl/vulkan/VulkanTest.java
+++ modules/lwjgl/vulkan/src/test/java/org/lwjgl/vulkan/VulkanTest.java
@@ -8,6 +8,7 @@ import org.lwjgl.system.*;
 import org.testng.annotations.*;
 
 import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 import static org.testng.Assert.*;
 
 @Test
@@ -44,6 +45,104 @@ public class VulkanTest {
             struct.flags(0x999999FF); // test masking
             assertEquals(struct.instanceShaderBindingTableRecordOffset(), 0xFFFFFF);
             assertEquals(struct.flags(), 0xFF);
+        }
+    }
+
+    public void testPNext() {
+        try (MemoryStack stack = stackPush()) {
+            VkDeviceCreateInfo base = VkDeviceCreateInfo.calloc(stack);
+
+            VkPhysicalDeviceDescriptorIndexingFeatures a = VkPhysicalDeviceDescriptorIndexingFeatures.calloc(stack);
+            VkPhysicalDevice8BitStorageFeatures        b = VkPhysicalDevice8BitStorageFeatures.calloc(stack);
+            VkPhysicalDevice16BitStorageFeatures       c = VkPhysicalDevice16BitStorageFeatures.calloc(stack);
+            VkPhysicalDeviceDynamicRenderingFeatures   d = VkPhysicalDeviceDynamicRenderingFeatures.calloc(stack);
+
+            // Approach 1: call pNext in reverse order, final order is base -> a -> b -> c -> d
+            // This is the LWJGL-recommended/idiomatic approach.
+
+            base
+                .pNext(d)
+                .pNext(c)
+                .pNext(b)
+                .pNext(a);
+
+            assertEquals(base.pNext(), a.address());
+            assertEquals(a.pNext(), b.address());
+            assertEquals(b.pNext(), c.address());
+            assertEquals(c.pNext(), d.address());
+            assertEquals(d.pNext(), NULL);
+
+            base.pNext(NULL);
+            a.pNext(NULL);
+            b.pNext(NULL);
+            c.pNext(NULL);
+            d.pNext(NULL);
+
+            // Approach 2: call pNext in normal order, final order is base -> d -> c -> b -> a
+            // This should work in practice, but leads to reversed order of pNext pointers,
+
+            base
+                .pNext(a)
+                .pNext(b)
+                .pNext(c)
+                .pNext(d);
+
+            assertEquals(base.pNext(), d.address());
+            assertEquals(d.pNext(), c.address());
+            assertEquals(c.pNext(), b.address());
+            assertEquals(b.pNext(), a.address());
+            assertEquals(a.pNext(), NULL);
+
+            base.pNext(NULL);
+            a.pNext(NULL);
+            b.pNext(NULL);
+            c.pNext(NULL);
+            d.pNext(NULL);
+
+            // Approach 3: use unsafe pNext calls, final order is base -> a -> b -> c -> d
+            // This is the WYSIWYG approach, but is more verbose.
+
+            a.pNext(b.address());
+            b.pNext(c.address());
+            c.pNext(d.address());
+            base.pNext(a.address());
+
+            assertEquals(base.pNext(), a.address());
+            assertEquals(a.pNext(), b.address());
+            assertEquals(b.pNext(), c.address());
+            assertEquals(c.pNext(), d.address());
+            assertEquals(d.pNext(), NULL);
+
+            base.pNext(NULL);
+            a.pNext(NULL);
+            b.pNext(NULL);
+            c.pNext(NULL);
+            d.pNext(NULL);
+
+            // Approach 4: use next unsafe pNext calls, final order is base -> a -> b -> c -> d
+            // This is another WYSIWYG approach, but is more verbose and ugly.
+
+            base
+                .pNext(a
+                    .pNext(b
+                        .pNext(c
+                            .pNext(d.address())
+                            .address())
+                        .address())
+                    .address()
+                );
+
+            assertEquals(base.pNext(), a.address());
+            assertEquals(a.pNext(), b.address());
+            assertEquals(b.pNext(), c.address());
+            assertEquals(c.pNext(), d.address());
+            assertEquals(d.pNext(), NULL);
+
+            base.pNext(NULL);
+            a.pNext(NULL);
+            b.pNext(NULL);
+            c.pNext(NULL);
+            d.pNext(NULL);
         }
     }
 
