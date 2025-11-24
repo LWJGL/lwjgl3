2cf36ca020a27c3d3a06fc533b772bbb73d6415e
Ioannis Tsakpinis <iotsakp@gmail.com>
2025-11-24 10:49:31 +0200

    build(core) update supported JDK releases
    Reminder, the LWJGL build system recognizes:
    
    * every LTS release starting with Java 8
    * every non-LTS release after the latest LTS release
    
    Older non-LTS releases will work fine, but will be recognized as the
    highest LTS version lower than the non-LTS version.

--- modules/ant/src/main/java/org/lwjgl/DetectJDKVersion.java
+++ modules/ant/src/main/java/org/lwjgl/DetectJDKVersion.java
@@ -6,17 +6,23 @@ package org.lwjgl;
 
 import org.apache.tools.ant.*;
 
+import java.util.*;
 import java.util.regex.*;
 
 public class DetectJDKVersion extends Task {
 
-    @Override public void execute() {
+    private static final Pattern JAVA_VERSION_PATTERN = Pattern.compile("^([1-9][0-9]*)(?:(?:\\.0)*\\.[1-9][0-9]*)*(?:-[a-zA-Z0-9]+)?");
 
+    private static final int[] LTS_RELEASES = {11, 17, 21, 25};
+
+    private static final int LTS_RELEASE_FIRST = LTS_RELEASES[0];
+    private static final int LTS_RELEASE_LAST  = LTS_RELEASES[LTS_RELEASES.length - 1];
+
+    @Override
+    public void execute() {
         String javaVersion = System.getProperty("java.version");
 
-        Matcher matcher = Pattern
-            .compile("^([1-9][0-9]*)(?:(?:\\.0)*\\.[1-9][0-9]*)*(?:-[a-zA-Z0-9]+)?")
-            .matcher(javaVersion);
+        Matcher matcher = JAVA_VERSION_PATTERN.matcher(javaVersion);
 
         if (!matcher.find()) {
             if (javaVersion.startsWith("1.")) {
@@ -28,10 +34,15 @@ public class DetectJDKVersion extends Task {
             throw new BuildException("Failed to parse java.version: " + javaVersion);
         }
 
+        Project LWJGL = getProject();
+
         int version = Integer.parseInt(matcher.group(1));
 
-        Project LWJGL = getProject();
-        for (int v = version; 9 <= v; v--) {
+        LWJGL.setProperty("jdk.version", Integer.toString(version));
+        for (int v = version; LTS_RELEASE_FIRST <= v; v--) {
+            if (v < LTS_RELEASE_LAST && Arrays.binarySearch(LTS_RELEASES, v) < 0) {
+                continue;
+            }
             LWJGL.setProperty("jdk" + v, "true");
         }
     }
