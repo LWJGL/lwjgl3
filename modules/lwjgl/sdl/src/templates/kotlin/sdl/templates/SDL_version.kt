c18b3db5cba04d5fc3b391e6d248e715a286a113
Ioannis Tsakpinis <iotsakp@gmail.com>
2025-12-07 16:32:16 +0200

    feat(sdl) update to 3.2.28

--- modules/lwjgl/sdl/src/templates/kotlin/sdl/templates/SDL_version.kt
+++ modules/lwjgl/sdl/src/templates/kotlin/sdl/templates/SDL_version.kt
@@ -10,7 +10,7 @@ import sdl.*
 val SDL_version = "SDLVersion".nativeClassSDL("SDL_version") {
     IntConstant("MAJOR_VERSION".."3")
     IntConstant("MINOR_VERSION".."2")
-    IntConstant("MICRO_VERSION".."26")
+    IntConstant("MICRO_VERSION".."28")
 
     int("GetVersion", void())
     charASCII.const.p("GetRevision", void())
