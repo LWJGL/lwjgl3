8d9f7055dbe8c9d278043bfcda755f0b94f78383
Sertonix <sertonix@posteo.net>
2026-02-02 11:56:03 +0100

    build(core) fix undefined reference while linking

--- modules/lwjgl/core/src/generated/c/org_lwjgl_system_libc_LibCStdlib.c
+++ modules/lwjgl/core/src/generated/c/org_lwjgl_system_libc_LibCStdlib.c
@@ -12,7 +12,7 @@
     #if defined(__USE_ISOC11)
         #define __aligned_alloc aligned_alloc
     #else
-        inline void* __aligned_alloc(size_t alignment, size_t size) {
+        static inline void* __aligned_alloc(size_t alignment, size_t size) {
             void *p;
             return posix_memalign(&p, alignment, size) ? NULL : p;
         }
