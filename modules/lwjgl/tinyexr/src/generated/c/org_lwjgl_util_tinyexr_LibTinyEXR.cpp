b1d93af87d1c055eb6bf0f04681db733d52a6285
Ioannis Tsakpinis <iotsakp@gmail.com>
2026-06-09 21:26:42 +0300

    feat(tinyexr) update to 3.0.0 (deleted only)
    Separate commit to avoid issues on case-insensitive file systems.

--- modules/lwjgl/tinyexr/src/generated/c/org_lwjgl_util_tinyexr_LibTinyEXR.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright LWJGL. All rights reserved.
- * License terms: https://www.lwjgl.org/license
- * MACHINE GENERATED FILE, DO NOT EDIT
- */
-#include "common_tools.h"
-#define LWJGL_MALLOC_LIB org_lwjgl_util_tinyexr_LibTinyEXR
-#include "lwjgl_malloc.h"
